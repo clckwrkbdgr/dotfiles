b4f2ffbd1e3e0e76c1ed7308576f08673c7d552f
Igor Chaika <kp580bm1@gmail.com>
2014-06-01 18:13:14 +0300

    Fixed making backup copies in setup script.

--- _setup.sh
+++ _setup.sh
@@ -6,6 +6,6 @@ HOMEDIR=~
 
 mkdir -p "$BACKUP_DOTDIR"
 for FILE in [^_]*; do
-	mv "$HOMEDIR/.$FILE" "$BACKUP_DOTDIR"
+	mv -b "$HOMEDIR/.$FILE" "$BACKUP_DOTDIR"
 	ln -s "${DOTDIR}/${FILE}" "$HOMEDIR/.$FILE"
 done
