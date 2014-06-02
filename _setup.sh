34aafb916375ac590fb26d042f336158398069ba
Igor Chaika <kp580bm1@gmail.com>
2014-06-02 10:15:43 +0300

    .config/fbless. _config dir for .config.

--- _setup.sh
+++ _setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DOTDIR=~/dotfiles
+DOTDIR_CONFIG=~/dotfiles/_config
 BACKUP_DOTDIR=~/dotfiles.bak
 HOMEDIR=~
 
@@ -13,3 +14,15 @@ for FILE in [^_]*; do
 		ln -s "${DOTDIR}/${FILE}" "$DEST" || echo "Error during copying .$FILE"
 	fi
 done
+
+mkdir -p "$BACKUP_DOTDIR/.config"
+pushd "$DOTDIR_CONFIG" >/dev/null
+for FILE in *; do
+	DEST="$HOMEDIR/.config/$FILE"
+	if [ -e "$DEST" ]; then
+		mv --backup=t "$DEST" "$BACKUP_DOTDIR/.config" && ln -s "${DOTDIR_CONFIG}/${FILE}" "$DEST" || echo "Error during copying .$FILE"
+	else
+		ln -s "${DOTDIR_CONFIG}/${FILE}" "$DEST" || echo "Error during copying .$FILE"
+	fi
+done
+popd >/dev/null
