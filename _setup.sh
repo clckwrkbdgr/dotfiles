fbce18bb27a4a7547ba84ca52ae4401616f17714
Igor Chaika <kp580bm1@gmail.com>
2014-06-02 09:38:03 +0300

    Fixed copying files when there's no existing ones in setup script.

--- _setup.sh
+++ _setup.sh
@@ -6,6 +6,10 @@ HOMEDIR=~
 
 mkdir -p "$BACKUP_DOTDIR"
 for FILE in [^_]*; do
-	mv -b "$HOMEDIR/.$FILE" "$BACKUP_DOTDIR"
-	ln -s "${DOTDIR}/${FILE}" "$HOMEDIR/.$FILE"
+	DEST="$HOMEDIR/.$FILE"
+	if [ -e "$DEST" ]; then
+		mv --backup=t "$DEST" "$BACKUP_DOTDIR" && ln -s "${DOTDIR}/${FILE}" "$DEST" || echo "Error during copying .$FILE"
+	else
+		ln -s "${DOTDIR}/${FILE}" "$DEST" || echo "Error during copying .$FILE"
+	fi
 done
