ee656999c307cac8ad92cc27b698a8cd1a55837c
Igor Chaika <kp580bm1@gmail.com>
2014-06-09 13:29:32 +0300

    Refactored _setup.sh script.

--- _setup.sh
+++ _setup.sh
@@ -6,37 +6,39 @@ DOTDIR_BIN=~/dotfiles/_bin
 BACKUP_DOTDIR=~/dotfiles.bak
 HOMEDIR=~
 
-mkdir -p "$BACKUP_DOTDIR"
-for FILE in [^_]*; do
-	DEST="$HOMEDIR/.$FILE"
-	if [ -e "$DEST" ]; then
-		mv --backup=t "$DEST" "$BACKUP_DOTDIR" && ln -s "${DOTDIR}/${FILE}" "$DEST" || echo "Error during copying .$FILE"
-	else
-		ln -s "${DOTDIR}/${FILE}" "$DEST" || echo "Error during copying .$FILE"
-	fi
-done
+get_abs_filename() {
+	REL_FILENAME=$1
+	echo "$(cd "$(dirname "$REL_FILENAME")" && pwd)/$(basename "$REL_FILENAME")"
+}
 
-mkdir -p "$BACKUP_DOTDIR/.config"
-pushd "$DOTDIR_CONFIG" >/dev/null
-for FILE in *; do
-	DEST="$HOMEDIR/.config/$FILE"
-	if [ -e "$DEST" ]; then
-		mv --backup=t "$DEST" "$BACKUP_DOTDIR/.config" && ln -s "${DOTDIR_CONFIG}/${FILE}" "$DEST" || echo "Error during copying .$FILE"
-	else
-		ln -s "${DOTDIR_CONFIG}/${FILE}" "$DEST" || echo "Error during copying .$FILE"
-	fi
-done
-popd >/dev/null
+function setup_files_from_dir()
+{
+	DIR_NAME=$1
+	ACTUAL_DIR_NAME=$2
 
-mkdir -p "$BACKUP_DOTDIR/bin"
-pushd "$DOTDIR_BIN" >/dev/null
-for FILE in *; do
-	echo $FILE
-	DEST="$HOMEDIR/bin/$FILE"
-	if [ -e "$DEST" ]; then
-		mv --backup=t "$DEST" "$BACKUP_DOTDIR/bin" && ln -s "${DOTDIR_BIN}/${FILE}" "$DEST" || echo "Error during copying .$FILE"
-	else
-		ln -s "${DOTDIR_BIN}/${FILE}" "$DEST" || echo "Error during copying .$FILE"
-	fi
-done
-popd >/dev/null
+	BACKUP_DIR_NAME=$(get_abs_filename "${BACKUP_DOTDIR}/${ACTUAL_DIR_NAME}")
+	DOTDIR_NAME=$(get_abs_filename "${DOTDIR}/${DIR_NAME}")
+	DEST_DIR_NAME=$(get_abs_filename "${HOMEDIR}/${ACTUAL_DIR_NAME}/")
+
+	echo "Installing <$DIR_NAME> to <$DEST_DIR_NAME>."
+	mkdir -p "$BACKUP_DIR_NAME"
+	pushd "$DOTDIR_NAME" >/dev/null
+	for FILE in [^_]*; do
+		DEST="${DEST_DIR_NAME}/${FILE}"
+		[ "$DIR_NAME" == '.' ] && DEST="${DEST_DIR_NAME}/.${FILE}"
+		DEST=$(get_abs_filename "$DEST")
+		DOTFILE_NAME=$(get_abs_filename "${DOTDIR_NAME}/${FILE}")
+
+		echo -e "\t$FILE"
+		if [ -e "$DEST" -o -h "$DEST" ]; then
+			mv --backup=t "$DEST" "$BACKUP_DIR_NAME" && ln -s "$DOTFILE_NAME" "$DEST" || echo "Error during copying '$FILE'"
+		else
+			ln -s "$DOTFILE_NAME" "$DEST" || echo "Error during copying .$FILE"
+		fi
+	done
+	popd >/dev/null
+}
+
+setup_files_from_dir . .
+setup_files_from_dir _config .config
+setup_files_from_dir _bin bin
