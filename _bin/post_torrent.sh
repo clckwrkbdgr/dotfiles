a79f2df5d423367660c550cd6d10b6f3a0b2783e
Igor Chaika <kp580bm1@gmail.com>
2015-11-21 09:49:51 +0200

    bin: changed path to sounds in several scripts

--- _bin/post_torrent.sh
+++ _bin/post_torrent.sh
@@ -6,4 +6,4 @@ POST_TR_LOG=~/.config/transmission/post_torrent.sh.log
 	ln -s "$TR_TORRENT_DIR/$TR_TORRENT_NAME" ~/"$TR_TORRENT_NAME" || (echo "failed to ln -s $TR_TORRENT_DIR/$TR_TORRENT_NAME ~/$TR_TORRENT_NAME"; env);
 ) &>>"$POST_TR_LOG"
 
-aplay -q /x/signal.wav
+aplay -q ~/owlwood/sounds/signal.wav
