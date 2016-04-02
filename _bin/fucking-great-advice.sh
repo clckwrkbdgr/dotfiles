c7adaa8e4f3db9c8c9e6e277e9225b2087d14fe4
Igor Chaika <kp580bm1@gmail.com>
2016-04-02 12:17:02 +0300

    bin/jenkins: doom, fortunes, knock, screenshot and various WM utils.

--- _bin/fucking-great-advice.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# fucking-great-advice.sh - prints random fortunes.
-
-/usr/bin/printf "$(wget -q -O - fucking-great-advice.ru/api/random | awk -F \" '{print $8}' | sed 's/\&nbsp;/ /g')\n" 
