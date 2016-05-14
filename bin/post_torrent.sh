6d3b9d6cc650512b1ed4bc26c653465a49058d1d
Igor Chaika <kp580bm1@gmail.com>
2016-05-14 22:38:59 +0300

    Revised bin scripts: removed some personal scripts, moved some scripts to bashrc as aliases, added comments to remaining scripts.

--- bin/post_torrent.sh
+++ bin/post_torrent.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-POST_TR_LOG=~/.config/transmission/post_torrent.sh.log
+# Creates link to finished torrent and places it to the home directory.
+# Also pops up a notification with name of the torrent.
+TRANSMISSION_DATA_DIR="$XDG_DATA_DIR/transmission"
+mkdir -p "$TRANSMISSION_DATA_DIR"
+POST_TR_LOG="$TRANSMISSION_DATA_DIR/post_torrent.sh.log"
 
 (
 	ln -s "$TR_TORRENT_DIR/$TR_TORRENT_NAME" ~/"$TR_TORRENT_NAME" || (echo "failed to ln -s $TR_TORRENT_DIR/$TR_TORRENT_NAME ~/$TR_TORRENT_NAME"; env);
 ) &>>"$POST_TR_LOG"
 
-~/.config/bin/xdg ~/.config/bin/johnny alert "Torrent $TR_TORRENT_NAME is finished."
+READABLE_NAME=$(echo "$TR_TORRENT_NAME" | sed 's/[.]/ /')
+~/.config/bin/xdg ~/.config/bin/johnny alert "Torrent $READABLE_NAME is finished."
