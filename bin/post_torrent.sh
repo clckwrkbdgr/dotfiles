9d5209045a1e650238f70e887a786807db739dd6
Dixie Flatline <umi0451@gmail.com>
2016-05-21 08:25:29 +0300

    post_torrent: fixed readable name construction.

--- bin/post_torrent.sh
+++ bin/post_torrent.sh
@@ -12,5 +12,5 @@ POST_TR_LOG="$TRANSMISSION_DATA_DIR/post_torrent.sh.log"
 	ln -s "$TR_TORRENT_DIR/$TR_TORRENT_NAME" ~/"$TR_TORRENT_NAME" || (echo "failed to ln -s $TR_TORRENT_DIR/$TR_TORRENT_NAME ~/$TR_TORRENT_NAME"; env);
 ) &>>"$POST_TR_LOG"
 
-READABLE_NAME=$(echo "$TR_TORRENT_NAME" | sed 's/[.]/ /')
+READABLE_NAME=$(echo "$TR_TORRENT_NAME" | sed 's/[.]/ /g')
 ~/.config/bin/xdg ~/.local/bin/johnny alert "Torrent $READABLE_NAME is finished."
