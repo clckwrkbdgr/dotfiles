2ad5c1c53e9ff7745082a0b0432945fba12b81ab
Dixie Flatline <umi0451@gmail.com>
2016-07-03 16:23:25 +0300

    firefox pref.js

---
+++ bin/clean-firefox-prefs.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+if [ "$1" = "--restore" ]; then
+	sed 's|$HOME|'"$HOME"'|g'
+	exit
+fi
+sed '/app[.]update[.]lastUpdateTime/d' | \
+sed '/browser[.]safebrowsing[.]provider[.]mozilla[.][^.]*updatetime/d' | \
+sed '/capability.policy.maonoscript.sites/d' | \
+sed '/datareporting.sessions.previous/d' | \
+sed '/devtools.scratchpad.recentFilePaths/d' | \
+sed '/extensions.bootstrappedAddons/d' | \
+sed '/extensions.downloadyoutubemp4[.]/d' | \
+sed '/extensions.enabled[^.]\+/d' | \
+sed '/extensions.firefox@ghostery.com.sdk.rootURI/d' | \
+sed '/extensions.sqlitemanager.jsonMruData/d' | \
+sed '/extensions.xpiState/d' | \
+sed '/print.print_to_filename/d' | \
+sed 's|'"$HOME"'|$HOME|g'
