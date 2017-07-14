f06b2b1c060409077c6480e58ede98431faa5a6e
Dixie Flatline <umi0451@gmail.com>
2017-07-14 16:28:05 +0300

    firefox: some pref update: colors, browsing, security.

--- bin/clean-firefox-prefs.sh
+++ bin/clean-firefox-prefs.sh
@@ -4,9 +4,10 @@ if [ "$1" = "--restore" ]; then
 	exit
 fi
 sed '/app[.]update[.]lastUpdateTime/d' | \
-sed '/browser[.]safebrowsing[.]provider[.]mozilla[.][^.]*updatetime/d' | \
+sed '/browser[.]safebrowsing[.]provider[.]\(mozilla\|google\)[.][^.]*updatetime/d' | \
 sed '/capability.policy.maonoscript.sites/d' | \
 sed '/datareporting.sessions.previous/d' | \
+sed '/datareporting.sessions.current/d' | \
 sed '/devtools.scratchpad.recentFilePaths/d' | \
 sed '/extensions.adblockplus.notificationdata/d' | \
 sed '/extensions.bootstrappedAddons/d' | \
@@ -24,6 +25,7 @@ sed '/extensions.blocklist.pingCount.*/d' | \
 sed '/extensions.getAddons.cache.lastUpdate/d' | \
 sed '/idle.lastDailyNotification/d' | \
 sed '/toolkit.startup.last_success/d' | \
+sed '/toolkit.telemetry.[^.]\+ID/d' | \
 sed '/storage.vacuum.last.places.sqlite/d' | \
 sed '/storage.vacuum.last.index/d' | \
 sed '/places.database.lastMaintenance/d' | \
@@ -64,4 +66,5 @@ sed '/\(extensions.\)\?e10s.rollout/d' | \
 sed '/extensions.webextensions.uuids/d' | \
 sed '/media.gmp[.-]/d' | \
 sed '/services.blocklist/d' | \
+sed '/browser.startup.homepage_override.torbrowser.version/d' | \
 sed 's|'"$HOME"'|$HOME|g'
