3ab27a9753ab4ae956d6d44b82386588c21c749a
Dixie Flatline <umi0451@gmail.com>
2017-07-29 09:04:02 +0300

    openttd: storing device-independent resolution setting using current desktop resolution.

--- bin/clean-openttd-cfg.sh
+++ bin/clean-openttd-cfg.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 if [ "$1" = "--restore" ]; then
+	XRESOLUTION=$(xrandr |grep \* |awk '{print $1}' | sed 's/x/,/')
 	sed 's|$HOME|'"$HOME"'|g'
+	sed 's|$XRESOLUTION|'"$XRESOLUTION"'|g'
 	exit
 fi
 sed '/^\[difficulty\]$/,/^\[.*\]$/{//!d}' | \
@@ -8,4 +10,5 @@ sed '/^\[game_creation\]$/,/^\[.*\]$/{//!d}' | \
 sed '/^station_numtracks *=/d' | \
 sed '/^station_platlength *=/d' | \
 sed '/^transparency_options *=/d' | \
+sed 's/^resolution *=.*/resolution = $XRESOLUTION/' | \
 sed 's|'"$HOME"'|$HOME|g'
