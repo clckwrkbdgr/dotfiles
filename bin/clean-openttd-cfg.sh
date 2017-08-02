84ead3d64cef097b8f5fd0bbedb481286ba7cf7f
Dixie Flatline <umi0451@gmail.com>
2017-08-02 19:17:36 +0300

    openttd: ignoring players custom window size.

--- bin/clean-openttd-cfg.sh
+++ bin/clean-openttd-cfg.sh
@@ -9,6 +9,8 @@ sed '/^\[difficulty\]$/,/^\[.*\]$/{//!d}' | \
 sed '/^\[game_creation\]$/,/^\[.*\]$/{//!d}' | \
 sed '/^station_numtracks *=/d' | \
 sed '/^station_platlength *=/d' | \
+sed 's/^pref_width *=.*$/pref_width = 0/' | \
+sed 's/^pref_height *=.*$/pref_height = 0/' | \
 sed '/^transparency_options *=/d' | \
 sed 's/^resolution *=.*/resolution = $XRESOLUTION/' | \
 sed 's|'"$HOME"'|$HOME|g'
