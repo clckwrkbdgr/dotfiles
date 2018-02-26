fea9c76e748298f0f33f3d82e665950d392566e6
Dixie Flatline <umi0451@gmail.com>
2018-02-26 21:51:35 +0200

    Unicode sample to test utf-8 and single-byte encoding for zipdump.

--- test/test_zipdump.sh
+++ test/test_zipdump.sh
@@ -9,10 +9,35 @@ Dolores sit amet
 EOF
 }
 
+function ryba_utf8() {
+cat <<EOF
+Лорем ипсум
+Долорес сит амет
+EOF
+}
+
 ### TESTS
 
 function should_zip_simple_text_file() {
 lorem_ipsum >'test.txt'
+echo $(cat 'test.txt' | wc -c)'||test.txt'
+cat 'test.txt'
+echo # Content separator.
+
+zip "test.zip" "test.txt" >&2
+}
+
+function should_zip_utf8() {
+ryba_utf8 >'test.txt'
+echo $(cat 'test.txt' | wc -c)'||test.txt'
+cat 'test.txt'
+echo # Content separator.
+
+zip "test.zip" "test.txt" >&2
+}
+
+function should_zip_cp1251() {
+ryba_utf8 | iconv -f 'utf-8' -t 'cp1251' >'test.txt'
 echo $(cat 'test.txt' | wc -c)'||test.txt'
 cat 'test.txt'
 echo # Content separator.
@@ -25,10 +50,19 @@ zip "test.zip" "test.txt" >&2
 testdir=$(mktemp -d)
 pushd "$testdir" >/dev/null
 
-should_zip_simple_text_file >"expected.txt"
-tput setf 4 # red
-zipdump dump "test.zip" | diff "expected.txt" - | cat -vet
-tput sgr0
+function perform_test() { # <test name>
+	"$1" >"expected.txt"
+	tput setf 4 # red
+	zipdump dump "test.zip" | diff "expected.txt" - | cat -vet
+	rc=$?
+	tput sgr0
+	rm -f "test.zip"
+	return "$rc"
+}
+
+perform_test should_zip_simple_text_file
+perform_test should_zip_utf8
+perform_test should_zip_cp1251
 
 popd >/dev/null
 rm -rf "$testdir"
