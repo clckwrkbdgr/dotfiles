5e66d9d3d559be84ec22260f8d0f3dd259d7505a
Dixie Flatline <umi0451@gmail.com>
2018-04-04 22:33:20 +0300

    Wrapper script for fancy grep with option to open found results as Vim quickfix.

---
+++ bin/search.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+perform_search() {
+	grep -RnI --exclude-dir=.wine --exclude-dir=.svn --exclude-dir=.git "$@"
+}
+
+args=()
+for arg in "$@"; do
+	if [ "$arg" == '-e' ]; then
+		open_editor=true
+	else
+		args+="$arg"
+	fi
+done
+
+
+if [ -n "$open_editor" ]; then
+	quickfix=/tmp/search.$$.quickfix
+	trap 'rm -f "$quickfix"' EXIT
+	perform_search "${args[@]}" | tee "$quickfix"
+	[ -s "$quickfix" ] && vim +cw -q "$quickfix"
+else
+	perform_search --color=auto "${args[@]}"
+fi
