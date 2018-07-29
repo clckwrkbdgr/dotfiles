810d98d1c6626028a57d5eb3e7d3f5a830bc4ae9
Dixie Flatline <umi0451@gmail.com>
2018-07-29 22:16:19 +0300

    Replacement action.

--- test/test_filterconf.sh
+++ test/test_filterconf.sh
@@ -61,6 +61,16 @@ function should_delete_regex_from_plain_text() {
 	echo -e "first\nsecond\nthird" >test.txt
 }
 
+function should_replace_substring_in_plain_text() {
+	echo -e 'first\n2nd\nthird' # Expected.
+	echo -e "first\nsecond\nthird" >test.txt
+}
+
+function should_replace_regex_in_plain_text() {
+	echo -e 'first\n2nd\nthird' # Expected.
+	echo -e "first\nsecond\nthird" >test.txt
+}
+
 ### MAIN
 
 testdir=$(mktemp -d)
@@ -68,38 +78,32 @@ pushd "$testdir" >/dev/null
 
 function perform_test() { # <format> <smudge|restore> <test name>
 	format="$1"
-	if [ "$2" == "smudge" ]; then
-		restore=""
-	elif [ "$2" == "restore" ]; then
-		restore="--restore"
-	else
-		echo "Expected smudge/restore, got: $2"
-		return 1
-	fi
-	echo ">>> $3"
-	"$3" >"expected.txt"
-	shift 3 # Make rooms for custom args.
+	echo ">>> $2"
+	"$2" >"expected.txt"
+	shift 2 # Make rooms for custom args.
 	tput setf 4 # red
 	export LANGUAGE=en_US:en # To make `diff` output 'printable'.
-	filterconf -f "$format" $restore "$@" <"test.$format" | diff "expected.txt" - | cat -vet
+	filterconf -f "$format" "$@" <"test.$format" | diff "expected.txt" - | cat -vet
 	rc=$?
 	tput sgr0
 	rm -f "test.$format"
 	return "$rc"
 }
 
-perform_test 'txt' 'smudge' should_smudge_home_dir_in_plain_text
-perform_test 'txt' 'restore' should_restore_home_dir_in_plain_text
-perform_test 'txt' 'smudge' should_smudge_custom_env_var -e 'XHOME=echo $HOME'
-perform_test 'txt' 'restore' should_restore_custom_env_var -e 'XHOME=echo $HOME'
-perform_test 'txt' 'smudge' should_smudge_several_custom_env_vars -e 'XHOME=echo $HOME' -e 'USERNAME=$USER'
-perform_test 'txt' 'restore' should_restore_several_custom_env_vars -e 'XHOME=echo $HOME' -e 'USERNAME=$USER'
+perform_test 'txt' should_smudge_home_dir_in_plain_text enviro
+perform_test 'txt' should_restore_home_dir_in_plain_text restore
+perform_test 'txt' should_smudge_custom_env_var -e 'XHOME=echo $HOME' enviro
+perform_test 'txt' should_restore_custom_env_var -e 'XHOME=echo $HOME' restore
+perform_test 'txt' should_smudge_several_custom_env_vars -e 'XHOME=echo $HOME' -e 'USERNAME=$USER' enviro
+perform_test 'txt' should_restore_several_custom_env_vars -e 'XHOME=echo $HOME' -e 'USERNAME=$USER' restore
 
-perform_test 'txt' 'smudge' should_sort_plain_text --sort
-perform_test 'txt' 'smudge' should_delete_value_from_plain_text --delete 'b'
-perform_test 'txt' 'smudge' should_delete_multiple_values_from_plain_text --delete 'b' 'c'
-perform_test 'txt' 'smudge' should_delete_line_with_substring_from_plain_text --delete 'eco'
-perform_test 'txt' 'smudge' should_delete_regex_from_plain_text --delete '^se.on+d$' --pattern-type 'regex'
+perform_test 'txt' should_sort_plain_text sort
+perform_test 'txt' should_delete_value_from_plain_text delete 'b'
+perform_test 'txt' should_delete_multiple_values_from_plain_text delete 'b' 'c'
+perform_test 'txt' should_delete_line_with_substring_from_plain_text delete 'eco'
+perform_test 'txt' should_delete_regex_from_plain_text delete '^se.on+d$' --pattern-type 'regex'
+perform_test 'txt' should_replace_substring_in_plain_text replace 'seco' --with '2'
+perform_test 'txt' should_replace_regex_in_plain_text replace '^seco([a-z]+)$' --pattern-type 'regex' --with '2\1'
 
 popd >/dev/null
 rm -rf "$testdir"
