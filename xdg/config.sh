a4b93070359f7dd0b2cecd8d1efdcf4b3fdb5b06
Dixie Flatline <umi0451@gmail.com>
2020-04-24 21:08:07 +0300

    Separated XDG profile into functional parts.

---
+++ xdg/config.sh
@@ -0,0 +1,10 @@
+# Customizations for XDG compliance: XDG_CONFIG_HOME
+export GIMP2_DIRECTORY="$XDG_CONFIG_HOME/gimp"
+export MPLAYER_HOME="$XDG_CONFIG_HOME/mplayer"
+export VIMPERATOR_INIT=":source $XDG_CONFIG_HOME/vimperator/vimperatorrc"
+export VIMPERATOR_RUNTIME="$XDG_CONFIG_HOME/vimperator"
+export VIMINIT='let $MYVIMRC="'"$XDG_CONFIG_HOME"'/vim/vimrc" | source $MYVIMRC'
+export MAILRC=~/.config/mailx/mailrc
+export PENTADACTYL_RUNTIME=$XDG_CONFIG_HOME/pentadactyl
+export GTK2_RC_FILES="$XDG_CONFIG_HOME/gtk-2.0/gtkrc"
+export XRE_PROFILE_PATH="$XDG_CONFIG_HOME/firefox"
---
+++ xdg/data.sh
@@ -0,0 +1,6 @@
+# Customizations for XDG compliance: XDG_DATA_HOME
+export GNUPGHOME="$XDG_DATA_HOME"/gnupg
+export ENCHANT_CONFIG_DIR="$XDG_DATA_HOME"/enchant
+export FREECIV_SAVE_PATH="$XDG_DATA_HOME"/freeciv/saves
+export FREECIV_SCENARIO_PATH="$XDG_DATA_HOME"/freeciv/scenarios:/usr/share/freeciv/scenarios:/usr/share/games/freeciv/scenarios:/usr/local/share/freeciv/scenarios
+export FREECIV_DATA_PATH="$XDG_DATA_HOME"/freeciv:/usr/share/freeciv:/usr/share/games/freeciv:/usr/local/share/freeciv
