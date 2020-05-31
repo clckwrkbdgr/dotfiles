0d566b8f0b59595c5689eb3b449ba3feacd1c721
Dixie Flatline <umi0451@gmail.com>
2020-05-31 21:16:15 +0300

    Deprecated xdg and crontab-env in favor of more generic user-env.cmd.

--- bin/user-env.cmd
+++ bin/user-env.cmd
@@ -1,5 +1,4 @@
 :<<@exit/b
-   @echo "crontab-env is deprecated, Use `$XDG_CONFIG_HOME/bin/user-env.cmd` instead"
    @%*
 @exit/b
    # Prepares user environment for user jobs in crontab.
@@ -9,6 +8,4 @@
    #   . ~/.config/bin/crontab-env.cmd; <command> <args...>
    export NO_SCREEN=true # To prevent terminal multiplexor. TODO - need a cross-shell way to detect non-interactive shell.
    . ~/.config/profile
-   . ~/.config/lib/utils.bash
-   deprecated 'Use `$XDG_CONFIG_HOME/bin/user-env.cmd` instead'
    "$@"
