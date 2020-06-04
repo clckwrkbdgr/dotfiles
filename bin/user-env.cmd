65398483d0f9039032e26b2aecf045c41b7cbd92
Dixie Flatline <umi0451@gmail.com>
2020-06-04 08:38:26 +0300

    Updated usage comment.

--- bin/user-env.cmd
+++ bin/user-env.cmd
@@ -1,11 +1,14 @@
 :<<@exit/b
    @%*
 @exit/b
-   # Prepares user environment for user jobs in crontab.
+   # Prepares full user environment, e.g. for user jobs in crontab.
+   # Re-read user profile each time, so can be used to run command
+   # in up-to-date environment without restart of underlying service,
+   # e.g. DE keybindings.
    # Usage for single command:
-   #   ~/.config/bin/crontab-env.cmd <command> <args...>
+   #   ~/.config/bin/user-env.cmd <command> <args...>
    # This file can also be sourced from Born-compatible shell:
-   #   . ~/.config/bin/crontab-env.cmd; <command> <args...>
+   #   . ~/.config/bin/user-env.cmd; <command> <args...>
    export NO_SCREEN=true # To prevent terminal multiplexor. TODO - need a cross-shell way to detect non-interactive shell.
    . ~/.config/profile
    "$@"
