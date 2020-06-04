01690b1e36d11323a39f864e322eaa4a243bd5e8
Dixie Flatline <umi0451@gmail.com>
2020-06-04 20:51:50 +0300

    Bash library for color manipulation.

---
+++ lib/colors.bash
@@ -0,0 +1,71 @@
+color::init() {
+	# Inits color mode.
+	# By default colors are on for TTY shells, and off for non-TTY.
+	# Call this function explicit args to force color mode on or off:
+	#   color::init [auto]
+	#   color::init on
+	#   color::init off
+
+	# TODO click
+	local color_mode="${1:-auto}"
+	if [ "$color_mode" == 'auto' ]; then
+		if [ -t 1 ]; then
+			color_mode='on'
+		else
+			color_mode='off'
+		fi
+	fi
+
+	if [ "$color_mode" == 'on' ]; then
+      black='\033[0;30m'
+        red='\033[0;31m'
+      green='\033[0;32m'
+     yellow='\033[0;33m'
+       blue='\033[0;34m'
+     purple='\033[0;35m'
+       cyan='\033[0;36m'
+      white='\033[0;37m'
+ bold_black='\033[1;30m'
+   bold_red='\033[1;31m'
+ bold_green='\033[1;32m'
+bold_yellow='\033[1;33m'
+  bold_blue='\033[1;34m'
+bold_purple='\033[1;35m'
+  bold_cyan='\033[1;36m'
+ bold_white='\033[1;37m'
+reset_color='\033[0m'
+	elif [ "$color_mode" == 'off' ]; then
+      black=''
+        red=''
+      green=''
+     yellow=''
+       blue=''
+     purple=''
+       cyan=''
+      white=''
+ bold_black=''
+   bold_red=''
+ bold_green=''
+bold_yellow=''
+  bold_blue=''
+bold_purple=''
+  bold_cyan=''
+ bold_white=''
+reset_color=''
+	else
+		echo "Unknown mode for color::init: <$color_mode>" >&2
+	fi
+}
+
+color::reset() {
+	# Resets both stdout and stderr color modes if there were any.
+	echo -en "${reset_color}"
+	echo -en "${reset_color}" >&2
+}
+
+color::strip() {
+	# Strips stdin from coloring escape sequences, returning only text.
+	sed "s%\x1b"'\(\[[0-9]\+\(;[0-9]\+\)\?m\|\][0-9]\+;\)%%g;s%\\\[\\\]%%g';
+}
+
+color::init auto
---
+++ lib/test/test_colors.bash
@@ -0,0 +1,49 @@
+. "$XDG_CONFIG_HOME/lib/unittest.bash"
+. "$XDG_CONFIG_HOME/lib/colors.bash"
+
+setUp() {
+	color::init on
+}
+
+tearDown() {
+	color::init auto
+	color::reset
+}
+
+should_use_colors_in_strings() {
+	assertStringsEqual "${red}the red in the sky is ours${reset_color}" \
+		'\033[0;31mthe red in the sky is ours\033[0m'
+}
+
+should_reset_previously_instated_color_mode() {
+	paint_it_red() {
+		echo -e "${red}Vision red..."
+		color::reset
+		echo -e "No more color, no more light."
+	}
+	assertOutputEqual paint_it_red "${red}Vision red...\n${reset_color}No more color, no more light."
+}
+
+should_force_init_colors() {
+	color::init off
+	assertStringsEqual "${red}Not red${reset_color}" 'Not red'
+	color::init on
+	assertOutputEqual 'echo -e "${red}Not red${reset_color}" | cat' '\033[0;31mNot red\033[0m'
+}
+
+should_autodetect_tty_for_colors() {
+	color::init off
+	assertStringsEqual "${red}" ''
+	color::init auto
+	assertStringsEqual "${red}" '\033[0;31m'
+	(
+		color::init auto
+		assertStringsEqual "${red}" ''
+	) | cat
+}
+
+should_strip_color_sequences_from_string() {
+	assertOutputEqual 'echo -e "${red}foo${blue}bar" | color::strip' 'foobar'
+}
+
+unittest::run should_
