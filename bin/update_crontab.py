ef11c86d0bcadca8099131eea1a02d6dbe6bb149
Dixie Flatline <umi0451@gmail.com>
2020-06-16 20:25:34 +0300

    Using XDG module for accessing user crontab locations.

--- bin/update_crontab.py
+++ bin/update_crontab.py
@@ -4,16 +4,18 @@ try:
 	from pathlib2 import Path
 except ImportError:
 	from pathlib import Path
+import clckwrkbdgr.xdg as xdg
 
-crontabs = [Path(_).expanduser() for _ in [
-	'~/.config/crontab', # TODO XDG
-	'~/.config/crontab.{0}'.format(platform.system()),
-	'~/.local/crontab',
-	'~/.local/crontab.{0}'.format(platform.system()),
-	'~/.local/crontab.{0}'.format(socket.gethostname()),
-	]]
+LOCAL = Path().home()/'.local'
+crontabs = [
+	xdg.XDG_CONFIG_HOME/'crontab',
+	xdg.XDG_CONFIG_HOME/'crontab.{0}'.format(platform.system()),
+	LOCAL/'crontab',
+	LOCAL/'crontab.{0}'.format(platform.system()),
+	LOCAL/'crontab.{0}'.format(socket.gethostname()),
+	]
 crontabs = b''.join(crontab.read_bytes() for crontab in crontabs if crontab.is_file())
 p = subprocess.Popen(['crontab'], stdin=subprocess.PIPE, shell=True)
 stdout, stderr = p.communicate(crontabs)
-assert not stdout and not stderr
+assert not stdout and not stderr, (stdout or b'') + (stderr or b'')
 sys.exit(p.wait())
