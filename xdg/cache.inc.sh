a2f22ed4e38b406bb80740fc06cffc9c67433bfe
Dixie Flatline <umi0451@gmail.com>
2020-07-02 21:19:40 +0300

    XDG for NPM.

--- xdg/cache.inc.sh
+++ xdg/cache.inc.sh
@@ -1,3 +1,4 @@
 # Customizations for XDG compliance: XDG_CACHE_HOME
 # These could easily be cleared at any time without loss of any important data.
 mkdir -p "$XDG_CACHE_HOME/vim"
+export NPM_CONFIG_CACHE="$XDG_CACHE_HOME/npm"
