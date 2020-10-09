8d5ad7b9b9d90ce83f7641d7012d445084f74e63
Dixie Flatline <umi0451@gmail.com>
2020-10-09 13:27:04 +0300

    Several aliases to replace missing actual utilities.

--- bash/aliases.inc.bash
+++ bash/aliases.inc.bash
@@ -9,3 +9,28 @@ alias isodate='date +%Y-%m-%dT%H:%M:%S'
 alias ranger-ctime='ranger --cmd="set sort ctime" --cmd="set sort_reverse true"'
 alias qiv='qiv -f -t -i'
 alias grep='grep --color=auto'
+which vimdiff >/dev/null 2>&1 || alias vimdiff='vim -d'
+
+function cdfind() {
+	D="$(dirname "$(find . -name "$@" | tee /dev/stderr | head -1)")"
+	[ -n "$D" ] && cd "$D"
+}
+export -f cdfind
+
+if ! which tree >/dev/null 2>&1; then
+	tree() {
+		find "${@:-.}" | sed -e 'sI/$II; sI[^/]*/I|- Ig; sI- |I  |Ig'
+	}
+	export -f tree
+else
+	alias tree='tree --charset=ascii'
+fi
+
+if ! which dos2unix >/dev/null 2>&1; then
+	dos2unix() {
+		tmp="/tmp/tmp-dos2unix-$RANDOM.tmp"
+		sed 's/$//g' <"$1" >"$tmp"
+		mv -f "$tmp" "$1"
+		rm -f "$tmp"
+	}
+fi
