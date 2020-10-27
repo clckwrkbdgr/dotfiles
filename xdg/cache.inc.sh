c374ac08f49a8dbfcdd1c5b917203fa883de19ba
Dixie Flatline <umi0451@gmail.com>
2020-10-27 10:54:17 +0200

    Ignoring errors when XDG_CACHE_HOME is not accessible.

--- xdg/cache.inc.sh
+++ xdg/cache.inc.sh
@@ -1,4 +1,4 @@
 # Customizations for XDG compliance: XDG_CACHE_HOME
 # These could easily be cleared at any time without loss of any important data.
-mkdir -p "$XDG_CACHE_HOME/vim"
+mkdir -p "$XDG_CACHE_HOME/vim" 2>/dev/null
 export NPM_CONFIG_CACHE="$XDG_CACHE_HOME/npm"
