b1e4ae214eedb4b3c14ae2548391f7ed58bbd8e8
Dixie Flatline <umi0451@gmail.com>
2020-10-28 07:40:15 +0200

    Fixed issue with "(new commits)" after updating submodules.

--- bin/dailyupdate.d/01.update_git_submodules.sh
+++ bin/dailyupdate.d/01.update_git_submodules.sh
@@ -2,3 +2,4 @@
 userstate get metered_network && exit
 cd "$XDG_CONFIG_HOME"
 git submodule update --init --remote --recursive --merge
+git submodule update --recursive # To auto-merge updated submodules with "(new commits)"
