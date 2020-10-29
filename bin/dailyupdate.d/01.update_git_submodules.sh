453ab8ddaa2a0fe504d389764d10de256e6b74eb
Dixie Flatline <umi0451@gmail.com>
2020-10-29 08:25:34 +0200

    Properly updating git submodules.

--- bin/dailyupdate.d/01.update_git_submodules.sh
+++ bin/dailyupdate.d/01.update_git_submodules.sh
@@ -2,4 +2,7 @@
 userstate get metered_network && exit
 cd "$XDG_CONFIG_HOME"
 git submodule update --init --remote --recursive --merge
-git submodule update --recursive # To auto-merge updated submodules with "(new commits)"
+git stash --keep-index
+	git submodule -q foreach 'echo $sm_path' | xargs git add
+	git commit -m 'Updated submodules.'
+git stash pop
