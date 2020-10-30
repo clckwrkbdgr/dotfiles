92994675c6bbdb32c26c0524bae01e819111698c
Dixie Flatline <umi0451@gmail.com>
2020-10-30 08:57:00 +0200

    Committing updated submodules only if there were updates at all.

--- bin/dailyupdate.d/01.update_git_submodules.sh
+++ bin/dailyupdate.d/01.update_git_submodules.sh
@@ -2,7 +2,9 @@
 userstate get metered_network && exit
 cd "$XDG_CONFIG_HOME"
 git submodule update --init --remote --recursive --merge
-git stash --keep-index
+if git submodule | grep -q '^+'; then # Having "(new commits)".
+	git stash --keep-index
 	git submodule -q foreach 'echo $sm_path' | xargs git add
 	git commit -m 'Updated submodules.'
-git stash pop
+	git stash pop
+fi
