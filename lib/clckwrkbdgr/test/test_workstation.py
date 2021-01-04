fe40c340f1a60988ef6a94ae1691e617c31d3f19
Dixie Flatline <umi0451@gmail.com>
2021-01-04 12:01:35 +0200

    Fixed unit tests on Windows.

--- lib/clckwrkbdgr/test/test_workstation.py
+++ lib/clckwrkbdgr/test/test_workstation.py
@@ -20,14 +20,15 @@ class TestWorkstationEvents(unittest.TestCase):
 	@mock.patch('clckwrkbdgr.commands.run_command_and_collect_output')
 	@mock.patch(pathlib_module + '.Path.is_file')
 	def should_run_arbitrary_event_script(self, is_file, run_command, save_data_path):
-		save_data_path.return_value = Path('/path/to/share/workstation')
+		mock_root_path = Path('/path/to/share/workstation')
+		save_data_path.return_value = mock_root_path
 		run_command.return_value = 0
 		is_file.return_value = True
 
 		result = workstation._run_event_script('myevent')
 		self.assertTrue(result)
 		save_data_path.assert_called_once_with('workstation')
-		run_command.assert_called_once_with([sys.executable, os.path.join('/path/to/share/workstation', 'myevent.py')])
+		run_command.assert_called_once_with([sys.executable, str(mock_root_path/'myevent.py')])
 	@mock.patch('clckwrkbdgr.xdg.save_data_path')
 	@mock.patch('clckwrkbdgr.commands.run_command_and_collect_output')
 	@mock.patch(pathlib_module + '.Path.is_file')
@@ -44,22 +45,23 @@ class TestWorkstationEvents(unittest.TestCase):
 	@mock.patch('clckwrkbdgr.commands.run_command_and_collect_output')
 	@mock.patch(pathlib_module + '.Path.is_file')
 	def should_recognize_default_events(self, is_file, run_command, save_data_path):
-		save_data_path.return_value = Path('/path/to/share/workstation')
+		mock_root_path = Path('/path/to/share/workstation')
+		save_data_path.return_value = mock_root_path
 		run_command.return_value = 0
 		is_file.return_value = True
 
 		run_command.reset_mock()
 		self.assertTrue(workstation.onlogin())
-		run_command.assert_called_once_with([sys.executable, os.path.join('/path/to/share/workstation', 'onlogin.py')])
+		run_command.assert_called_once_with([sys.executable, str(mock_root_path/'onlogin.py')])
 
 		run_command.reset_mock()
 		self.assertTrue(workstation.onlogout())
-		run_command.assert_called_once_with([sys.executable, os.path.join('/path/to/share/workstation', 'onlogout.py')])
+		run_command.assert_called_once_with([sys.executable, str(mock_root_path/'onlogout.py')])
 
 		run_command.reset_mock()
 		self.assertTrue(workstation.onlock())
-		run_command.assert_called_once_with([sys.executable, os.path.join('/path/to/share/workstation', 'onlock.py')])
+		run_command.assert_called_once_with([sys.executable, str(mock_root_path/'onlock.py')])
 
 		run_command.reset_mock()
 		self.assertTrue(workstation.onunlock())
-		run_command.assert_called_once_with([sys.executable, os.path.join('/path/to/share/workstation', 'onunlock.py')])
+		run_command.assert_called_once_with([sys.executable, str(mock_root_path/'onunlock.py')])
