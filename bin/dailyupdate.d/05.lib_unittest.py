3193b10e7deec4640d10af2a850ae892ef9cf0fd
Dixie Flatline <umi0451@gmail.com>
2021-02-08 14:19:27 +0200

    Windows cannot locate shebang-based scripts by default.

--- bin/dailyupdate.d/05.lib_unittest.py
+++ bin/dailyupdate.d/05.lib_unittest.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 import os, sys, subprocess
+import platform
 from clckwrkbdgr import xdg
 
 os.chdir(str(xdg.XDG_CONFIG_HOME/'lib'))
 args = ['unittest']
 if not os.environ.get('DAILYUPDATE_VERBOSE'):
 	args.append("-q")
-sys.exit(subprocess.call(args))
+sys.exit(subprocess.call(args, shell=(platform.system()=='Windows')))
