d56f1f5c3d93039dbc02d6594d9418e56f3bdc52
Dixie Flatline <umi0451@gmail.com>
2021-05-06 09:45:43 +0300

    Context for backup operation.

--- lib/clckwrkbdgr/backup.py
+++ lib/clckwrkbdgr/backup.py
@@ -1,4 +1,9 @@
+import os, tempfile
 import contextlib
+try:
+	from pathlib2 import Path
+except ImportError: # pragma: no cover
+	from pathlib import Path
 
 class Password(object):
 	""" Encapsulates password so the actual value would not be present in stack traces.
@@ -43,3 +48,62 @@ class PasswordArg(object):
 		return '-p{0}'.format(self.password)
 	def __repr__(self):
 		return repr(self.password)
+
+class Config(object):
+	""" Context for backup operation:
+	- root: source directory for backup operations.
+	  Tilde is recognized.
+	  If path is not absolute, it is treated as relative to the current directory
+	  (which should be directory of config file).
+	  The only required parameter.
+	- name: of the produced backup file.
+	  By default will use basename of the root.
+	- destinations: List of destination remote locations.
+	  Environment variables and tilde are recognized.
+	  By default produced backup will not be deployed anywhere.
+	- tempdir: Temp dir for creating backup.
+	  Should be absolute path. Tilde is recognized.
+	  Default is system tempdir.
+	- zip_path: Path to 7zip executable.
+	  Should be absolute and resolved path.
+	  By default will try to auto-detect installed 7zip in the system.
+	- password: password to protect archive.
+	  By default password is disabled.
+	  Stored as protected Password object.
+	  If password is a dict:
+	  - { "file" : "<path to file with password>" }
+	    Reads password from specified file.
+		 Filename may contain environment variables and tilde.
+	    File should contain a single line with optional EOL (it will be stripped automatically).
+	- exclude: List of excluded directories and/or files.
+	  May be wildcards.
+	  By default all files are included.
+	"""
+	def __init__(self,
+			root=None,
+			name=None,
+			destinations=None, tempdir=None, zip_path=None,
+			password=None,
+			exclude=None,
+			): # pragma: no cover -- TODO accesses FS, needs proper mocks for FS and Path object.
+		""" Creates context for backup operation.
+		See main docstring for details on each parameter.
+		"""
+		self.root = Path(root).expanduser()
+		self.name = name or self.root.name
+		self.destinations = [Path(os.path.expandvars(location)).expanduser() for location in (destinations or [])]
+		self.tempdir = Path(tempdir or tempfile.gettempdir()).expanduser()
+		self.zip_path = Path(zip_path or r"C:\Program Files\7-Zip\7z.exe")
+		self.exclude = list(exclude or [])
+		if isinstance(password, dict):
+			if 'file' in password:
+				try:
+					password = password['file']
+					password = Path(os.path.expandvars(password)).expanduser().read_text()
+					if password.endswith('\n'):
+						password = password[:-1]
+				except Exception as e:
+					raise ValueError('Failed to read password file: {0}'.format(e))
+			else:
+				raise ValueError('Unknown password field configuration! Supported configurations are: plain string, {.file=<...>}')
+		self.password = Password(password)
