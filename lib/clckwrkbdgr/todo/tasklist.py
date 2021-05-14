7062e85313392071fdfebd367163b4dde1b00fd8
Dixie Flatline <umi0451@gmail.com>
2021-05-14 16:06:58 +0300

    Option to maintain order in task list. Command to sort task list by editing task list file directly.

--- lib/clckwrkbdgr/todo/tasklist.py
+++ lib/clckwrkbdgr/todo/tasklist.py
@@ -1,5 +1,7 @@
-import sys
+import sys, subprocess
 import importlib
+import logging
+from collections import namedtuple
 import clckwrkbdgr.todo
 import clckwrkbdgr.todo.provider
 from clckwrkbdgr import xdg
@@ -10,17 +12,59 @@ for entry in clckwrkbdgr.todo.read_config().task_providers: # pragma: no cover -
 	except Exception as e:
 		print("Failed to load task provider '{0}': {1}".format(entry, e))
 
+def filter_task_list(original, current):
+	""" Checks each line from original list against current.
+	Yields pairs (bool, <line>), where bool is True, if line should be kept, and False if line should be removed, and None if it is new line.
+	"""
+	prev = None
+	for line in current:
+		if line not in original:
+			yield None, line
+			prev = line
+	for line in original:
+		if not line.strip():
+			if prev is None:
+				logging.debug("Empty line at the beginning, skipping.")
+				yield False, line
+			elif not prev.strip():
+				logging.debug("Consequent empty line, skipping.")
+				yield False, line
+			else:
+				yield True, line
+				prev = line
+		elif line not in current:
+			logging.debug("Task is not present anymore: {0}".format(line))
+			yield False, line
+		else:
+			yield True, line
+			prev = line
+
 class TaskList: # pragma: no cover -- TODO depends on global list of task providers, also on FS.
 	def __init__(self):
 		self._filename = xdg.save_state_path('todo')/'tasklist.lst'
+	def sort(self):
+		config = clckwrkbdgr.todo.read_config()
+		return 0 == subprocess.call(config.editor + [self._filename], shell=True)
 	def sync(self):
-		new_lines = []
+		tasks = []
 		for provider in clckwrkbdgr.todo.task_provider:
 			for task in provider():
-				new_lines.append(task.title)
-		self._filename.write_text('\n'.join(new_lines) + '\n')
-	def list_all(self):
+				tasks.append(task.title.encode('utf-8', 'replace'))
+
+		old_lines = self._filename.read_bytes().splitlines()
+		new_lines = []
+		changed = False
+		for keep, line in filter_task_list(old_lines, tasks):
+			if keep or keep is None:
+				new_lines.append(line)
+			if not keep:
+				changed = True
+		if changed:
+			self._filename.write_bytes(b'\n'.join(new_lines).rstrip() + b'\n')
+	def list_all(self, with_seps=False):
 		if not self._filename.exists():
 			return
 		for line in self._filename.read_text(errors='replace').splitlines():
+			if not with_seps and not line.strip():
+				continue
 			yield line
