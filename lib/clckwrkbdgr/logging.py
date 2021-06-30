2b1997a67b0a011c62717d6fae27e964455755cf
Dixie Flatline <umi0451@gmail.com>
2021-06-30 16:25:36 +0300

    Logging utilities.

---
+++ lib/clckwrkbdgr/logging.py
@@ -0,0 +1,6 @@
+import logging
+
+def getFileLogger(name, filename): # pragma: no cover
+	logger = logging.getLogger(name)
+	logger.addHandler(logging.FileHandler(str(filename), delay=True))
+	return logger
