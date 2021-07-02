419ad315bf1ff63f8f7c3f4ef83ddf3ead18a71d
Dixie Flatline <umi0451@gmail.com>
2021-07-02 12:10:30 +0300

    Ignore own configuration file when todo dir has default value.

--- lib/clckwrkbdgr/todo/provider/todo_dir.py
+++ lib/clckwrkbdgr/todo/provider/todo_dir.py
@@ -3,10 +3,13 @@ try:
 	from pathlib2 import Path
 except ImportError: # pragma: no cover
 	from pathlib import Path
+from clckwrkbdgr import xdg
 
 @clckwrkbdgr.todo.task_provider('todo_dir')
 def list_todo_directory(): # pragma: no cover -- TODO accesses FS and config directly.
 	for entry in clckwrkbdgr.todo.read_config().todo_dir.iterdir():
 		if entry == clckwrkbdgr.todo.read_config().inbox_file:
+			continue
+		if entry == xdg.save_data_path('todo')/'config.json':
 			continue
 		yield clckwrkbdgr.todo.Task(entry.name)
