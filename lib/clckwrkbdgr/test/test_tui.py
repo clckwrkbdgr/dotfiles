88e9005c8c33f9143f546e8f85b6b1f4c6294e46
Dixie Flatline <umi0451@gmail.com>
2021-08-09 21:23:23 +0300

    Utilities for TUI.

---
+++ lib/clckwrkbdgr/test/test_tui.py
@@ -0,0 +1,63 @@
+import unittest
+unittest.defaultTestLoader.testMethodPrefix = 'should'
+import textwrap
+from clckwrkbdgr import tui
+
+class TestKey(unittest.TestCase):
+	def should_create_key(self):
+		self.assertEqual(tui.Key(65).value, 65)
+		self.assertEqual(tui.Key('A').value, 65)
+		self.assertEqual(tui.Key(tui.Key('A')).value, 65)
+	def should_compare_keys(self):
+		self.assertTrue(tui.Key('A') == 65)
+		self.assertTrue(65 == tui.Key('A'))
+		self.assertTrue('A' == tui.Key('A'))
+		self.assertTrue(tui.Key('A') == 'A')
+		self.assertTrue(tui.Key('A') == tui.Key('A'))
+		self.assertTrue(tui.Key('A') != 64)
+		self.assertTrue(tui.Key('A') < 'B')
+		self.assertTrue(tui.Key('A') <= 'A')
+		self.assertTrue(tui.Key('A') > ' ')
+		self.assertTrue(tui.Key('A') in ['A'])
+		self.assertTrue(tui.Key('A') in {tui.Key('A')})
+
+class TestUtils(unittest.TestCase):
+	def should_fit_text_into_screen(self):
+		lorem_ipsum = textwrap.dedent("""\
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+				sed do eiusmod tempor incididunt ut labore et dolore magna
+				aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+				ullamco laboris nisi ut aliquip ex ea commodo consequat.
+				Duis aute irure dolor in reprehenderit in voluptate velit
+				esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+				occaecat cupidatat non proident, sunt in culpa qui officia
+				deserunt mollit anim id est laborum.
+				""")
+		expected = [
+				'Lorem ipsum dolor sit amet',
+				'sed do eiusmod tempor inci',
+				'aliqua. Ut enim ad minim v',
+				'[...]',
+				'esse cillum dolore eu fugi',
+				'occaecat cupidatat non pro',
+				'deserunt mollit anim id es',
+			]
+		self.assertEqual(tui.ExceptionScreen._fit_into_bounds(lorem_ipsum, len(expected[0]), len(expected)), expected)
+		lorem_ipsum = textwrap.dedent("""\
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+				sed do eiusmod tempor incididunt ut labore et dolore magna
+				aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+				ullamco laboris nisi ut aliquip ex ea commodo consequat.
+				Duis aute irure dolor in reprehenderit in voluptate velit
+				esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+				(CUT      to make odd number of lines)
+				""")
+		expected = [
+				'Lorem ipsum dolor sit amet',
+				'sed do eiusmod tempor inci',
+				'[...]',
+				'Duis aute irure dolor in r',
+				'esse cillum dolore eu fugi',
+				'(CUT      to make odd numb',
+			]
+		self.assertEqual(tui.ExceptionScreen._fit_into_bounds(lorem_ipsum, len(expected[0]), len(expected)), expected)
---
+++ lib/clckwrkbdgr/tui.py
@@ -0,0 +1,73 @@
+from __future__ import division
+import sys
+import functools
+try:
+	import curses
+except: # pragma: no cover
+	curses = None
+import six
+
+@functools.total_ordering
+class Key(object):
+	""" Wrapper for getch'ed key.
+	Unifies operations (initialization, comparison) for numeric value (key's integer value), string value (key's char):
+	Key('A') == Key(65) == 'A' == 65
+	"""
+	def __init__(self, value):
+		if isinstance(value, Key):
+			self.value = value.value
+		elif isinstance(value, six.string_types):
+			self.value = ord(value)
+		else:
+			self.value = int(value)
+	def __hash__(self):
+		return hash(self.value)
+	def __str__(self): # pragma: no cover
+		return chr(self.value)
+	def __repr__(self): # pragma: no cover
+		return 'Key({0} {1})'.format(self.value, repr(chr(self.value)))
+	def __lt__(self, other):
+		other = Key(other)
+		return self.value < other.value
+	def __eq__(self, other):
+		other = Key(other)
+		return self.value == other.value
+
+class ExceptionScreen(object):
+	""" Context manager that captures exceptions and displays traceback in window overlay,
+	prompting to press a key to either exit immediately (quit_key, raises sys.exit) or proceed (any other).
+	"""
+	def __init__(self, window, quit_key='Q', prompt='[Press <Q> to quit, any other key to continue]'): # pragma: no cover
+		self.window = window
+		self.quit_key = Key(quit_key)
+		self.prompt = prompt
+	def __enter__(self): # pragma: no cover
+		return self
+	@staticmethod
+	def _fit_into_bounds(text, width, height):
+		""" Split text into lines and returns parts fitting into specified bounds. """
+		lines = [line.replace('\t', '  ')[:width] for line in text.splitlines()]
+		if len(lines) > height:
+			top_part = bottom_part = height // 2
+			top_part -= 1
+			while top_part + 1 + bottom_part < height:
+				top_part += 1
+			print(height, top_part, bottom_part)
+			lines = lines[:top_part] + ['[...]'] + lines[-bottom_part:]
+		return lines
+	def __exit__(self, exc_type, exc_value, exc_traceback): # pragma: no cover -- TUI
+		if exc_type is None:
+			return
+		import traceback
+		height, width = self.window.getmaxyx()
+		lines = self._fit_into_bounds(traceback.format_exc(), width, height - 1)
+		lines += [self.prompt[:width - 1]] # Skip the very bottom-right character because it will move cursor and trigger curses out-of-screen exception.
+
+		self.window.clear()
+		for row, line in enumerate(lines):
+			self.window.addstr(row, 0, line)
+		self.window.refresh()
+		if self.window.getch() == self.quit_key:
+			sys.exit()
+		self.window.clear()
+		return True # Ignore exception and proceed.
