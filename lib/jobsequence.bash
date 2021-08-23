80b2c5d17f93eadcf355c25c95474112bd11dbfb
Dixie Flatline <umi0451@gmail.com>
2021-08-23 08:45:06 +0300

    Bash library for jobsequence scripts.

---
+++ lib/jobsequence.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Set of utilities for jobsequence scripts.
+# Mostly repeats jobsequence.context and jobsequence.script for shell-based jobsequence scripts.
+. "$XDG_CONFIG_HOME/lib/miniclick.bash"
+
+jobsequence_context() {
+	# Initiates context for jobsequence script.
+	miniclick --script_rootdir -- "$@"
+	#   script_rootdir - default directory where fixer scripts will be created.
+	export JOBSEQUENCE_SCRIPT_ROODIR="${script_rootdir:-$HOME}"
+}
+
+jobsequence_script() {
+	# Prepares fixer script with given name and optional shebang line.
+	# Prints created filename to stdout.
+	miniclick script_name shebang -- -- "$@"
+
+	filename="${JOBSEQUENCE_SCRIPT_ROODIR:-.}/$filename"
+	mkdir -p "$(dirname "$filename")"
+	touch "$filename"
+	chmod +x "$filename"
+	echo "=== Created file $filename" >&2
+	if [ -n "$shebang" ]; then
+		echo "$shebang" >> "$filename"
+	fi
+	echo "$filename"
+}
