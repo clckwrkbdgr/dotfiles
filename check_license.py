f9d648fefe3c41f3d6d2a5f866054cd17a4506d5
Dixie Flatline <umi0451@gmail.com>
2021-08-25 11:16:40 +0300

    Option to search in versioned content instead of FS working copy. Added some more license patterns to detect.

--- check_license.py
+++ check_license.py
@@ -1,8 +1,11 @@
 """ Searches for any specification of license in given path(s) (recursively).
 If path is not given, current path is used.
+Special case: if path is exactly '.git' and CWD is a root of Git repo, search is performed in
+Git-versioned content instead (bypassing working copy modifications/textconv).
+
 Prints found resuls in in grep-like format '<filename>:<ln>:<found patterns>:<line>'
 """
-import sys, os, re
+import sys, os, re, subprocess
 
 def list_files(path):
 	if os.path.isfile(path):
@@ -13,30 +16,49 @@ def list_files(path):
 		for filename in filenames:
 			yield os.path.join(root, filename)
 
-def grep(filename, patterns):
+def fs_content(filename):
 	with open(filename, 'rb') as f:
-		for line_number, line in enumerate(f):
-			line = line.decode('utf-8', 'replace')
-			for pattern in patterns:
-				matches = pattern.findall(line)
-				if matches:
-					line = line.rstrip()[:120]
-					yield line_number, ','.join(map(str, matches)) + ':' + line
+		for line in f:
+			yield line
 
-def search_in_files(path):
-	patterns = [re.compile(pattern, flags=re.I) for pattern in [
-		r'GPL',
+def git_ls_files(_):
+	submodules = subprocess.check_output(['git', 'config', '--file', '.gitmodules', '--name-only', '--get-regexp', '[.]path$']).decode('utf-8', 'replace').splitlines()
+	submodules = [line.split('.', 1)[1].rsplit('.', 1)[0] for line in submodules]
+	for entry in subprocess.check_output(['git', 'ls-files']).decode('utf-8', 'replace').splitlines():
+		if entry in submodules:
+			continue
+		yield entry
+
+def git_show(filename):
+	for line in subprocess.check_output(['git', 'show', 'HEAD:{0}'.format(filename)]).splitlines():
+		yield line
+
+def grep(byte_lines, patterns):
+	for line_number, line in enumerate(byte_lines):
+		line = line.decode('utf-8', 'replace')
+		for pattern in patterns:
+			matches = pattern.findall(line)
+			if matches:
+				line = line.rstrip()[:120]
+				yield line_number, ','.join(map(str, matches)) + ':' + line
+
+PATTERNS = [re.compile(pattern, flags=re.I) for pattern in [
+		r'\bMIT',
+		r'\bMPL',
+		r'\bGPL',
 		r'licen[sc]e',
 		]]
-	for filename in list_files(path):
-		if filename == __file__:
-			continue
-		for line_number, text in grep(filename, patterns):
-			yield '{0}:{1}:{2}'.format(filename, line_number, text)
 
 if __name__ == '__main__':
 	args = sys.argv[1:] or ['.']
-	for arg in args:
-		for result in search_in_files(arg):
-			print(result)
+	for path in args:
+		searcher, catter = list_files, fs_content
+		if path == '.git' and os.path.exists('.git'):
+			searcher, catter = git_ls_files, git_show
+
+		for filename in searcher(path):
+			if filename == __file__:
+				continue
+			for line_number, text in grep(catter(filename), PATTERNS):
+				print('{0}:{1}:{2}'.format(filename, line_number, text))
 
