a54045ddc8cfc28f8e38a21ff62ccffd44c099be
Dixie Flatline <umi0451@gmail.com>
2021-08-25 11:26:49 +0300

    Changed option format. Added option to list specific paths from Git.

--- check_license.py
+++ check_license.py
@@ -1,6 +1,6 @@
 """ Searches for any specification of license in given path(s) (recursively).
 If path is not given, current path is used.
-Special case: if path is exactly '.git' and CWD is a root of Git repo, search is performed in
+If option --git is given and CWD is a root of Git repo, search is performed in
 Git-versioned content instead (bypassing working copy modifications/textconv).
 
 Prints found resuls in in grep-like format '<filename>:<ln>:<found patterns>:<line>'
@@ -8,6 +8,7 @@ Prints found resuls in in grep-like format '<filename>:<ln>:<found patterns>:<li
 import sys, os, re, subprocess
 
 def list_files(path):
+	path = path or '.'
 	if os.path.isfile(path):
 		yield path
 		return
@@ -21,10 +22,12 @@ def fs_content(filename):
 		for line in f:
 			yield line
 
-def git_ls_files(_):
+def git_ls_files(path):
 	submodules = subprocess.check_output(['git', 'config', '--file', '.gitmodules', '--name-only', '--get-regexp', '[.]path$']).decode('utf-8', 'replace').splitlines()
 	submodules = [line.split('.', 1)[1].rsplit('.', 1)[0] for line in submodules]
 	for entry in subprocess.check_output(['git', 'ls-files']).decode('utf-8', 'replace').splitlines():
+		if path and not entry.startswith(path):
+			continue
 		if entry in submodules:
 			continue
 		yield entry
@@ -50,12 +53,14 @@ PATTERNS = [re.compile(pattern, flags=re.I) for pattern in [
 		]]
 
 if __name__ == '__main__':
-	args = sys.argv[1:] or ['.']
-	for path in args:
-		searcher, catter = list_files, fs_content
-		if path == '.git' and os.path.exists('.git'):
-			searcher, catter = git_ls_files, git_show
+	args = sys.argv[1:] or [None]
+	searcher, catter = list_files, fs_content
+	if '--git' in args:
+		if not os.path.exists('.git'):
+			raise RuntimeError('Cannot use option --git in non-Git repo root.')
+		searcher, catter = git_ls_files, git_show
 
+	for path in args:
 		for filename in searcher(path):
 			if filename == __file__:
 				continue
