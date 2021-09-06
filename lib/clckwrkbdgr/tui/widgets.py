9bd5a28c1bb4f13b5a6f0ab5cc41ce64244343d3
Dixie Flatline <umi0451@gmail.com>
2021-09-06 15:28:10 +0300

    Simple menu.

--- lib/clckwrkbdgr/tui/widgets.py
+++ lib/clckwrkbdgr/tui/widgets.py
@@ -1,4 +1,6 @@
+from collections import namedtuple
 from . import app
+import curses
 
 class TextScreen(app.MVC): # pragma: no cover -- TODO curses
 	""" Displays visual notification on new screen
@@ -53,4 +55,44 @@ class Confirmation(app.ModalMVC): # pragma: no cover -- TODO curses
 		if self._responded:
 			return not None
 		self._responded = True
+		return None
+
+class Menu(app.MVC): # pragma: no cover -- TODO curses
+	""" Custom menu with items controlled by specific hot keys.
+	Following options to override:
+	- items(): list of Menu.Item(key, text)
+	- KEYS_TO_CLOSE: keys that will immediately close menu and return to the mode specifed in on_close(). Default is ESC only.
+	- on_close(): called when menu is simply closed. Should return new MVC.
+	- on_item(item): called when item is selected.
+	  Return value is treated as MVC mode: if None, continues with current menu,
+	  otherwise starts new mode.
+	WARNING: Scroll is not supported yet! Only items that fit into screen will be displayed.
+	"""
+	_full_redraw = True
+	Item = namedtuple('MenuItem', 'key text data')
+
+	KEYS_TO_CLOSE = [curses.ascii.ESC]
+	def items(self):
+		raise NotImplementedError
+	def on_item(self, item):
+		return None
+	def on_close(self):
+		return None
+
+	def _view(self, window):
+		height, width = window.getmaxyx()
+		height -= 1 # For the top line.
+		items = list(enumerate(self.items()))
+		if len(items) >= height:
+			items = items[:height-1] + [ (height-1, '[{0} more items do not fit...]'.format(len(items) - height + 1)) ]
+		for row, item in items:
+			line = "{0} - {1}".format(item.key, item.text)
+			line = line[:width-1]
+			window.addstr(1 + row, 0, line)
+	def _control(self, ch):
+		if any(ch == _ for _ in self.KEYS_TO_CLOSE):
+			return self.on_close()
+		for item in self.items():
+			if ch == ord(item.key):
+				return self.on_item(item)
 		return None
