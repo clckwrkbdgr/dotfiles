05e966af8ed3a02760e7b84e1891b24c846109b9
Dixie Flatline <umi0451@gmail.com>
2021-09-23 08:53:29 +0300

    Fixed imports.

--- lib/clckwrkbdgr/winnt/__init__.py
+++ lib/clckwrkbdgr/winnt/__init__.py
@@ -22,10 +22,10 @@ def IsRemoteSession(): # pragma: no cover -- TODO WinApi call
 	# You should not use GetSystemMetrics(SM_REMOTESESSION) to determine if your application is running in a remote session in Windows 8 and later or Windows Server 2012 and later if the remote session may also be using the RemoteFX vGPU improvements to the Microsoft Remote Display Protocol (RDP). In this case, GetSystemMetrics(SM_REMOTESESSION) will identify the remote session as a local session.
 	# Your application can check the following registry key to determine whether the session is a remote session that uses RemoteFX vGPU. If a local session exists, this registry key provides the ID of the local session.
 	#    HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Terminal Server\GlassSessionId
-	user32 = ctypes.windll.user32
+	user32 = windll.user32
 	SM_REMOTESESSION = 0x1000
 	return user32.GetSystemMetrics(SM_REMOTESESSION)
 
 def SwapMouseButton(do_swap): # pragma: no cover -- TODO WinApi call
-	user32 = ctypes.windll.user32
+	user32 = windll.user32
 	return user32.SwapMouseButton(1 if do_swap else 0)
