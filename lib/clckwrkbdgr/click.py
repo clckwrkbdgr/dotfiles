8d7f941c6930907a9d595c69eaecc504dca3b5bf
Dixie Flatline <umi0451@gmail.com>
2021-09-24 13:17:56 +0300

    Fixing issues with auto-expanding Click arguments on Windows by default.

--- lib/clckwrkbdgr/click.py
+++ lib/clckwrkbdgr/click.py
@@ -12,3 +12,23 @@ class RawEpilogGroup(click.Group): # pragma: no cover
 			formatter.write_paragraph()
 			for line in self.epilog.split('\n'):
 				formatter.write_text(line)
+
+class windows_noexpand_args: # pragma: no cover
+	""" Fixes backward-incompatible issues introduced by click authors in new versions.
+	May serve as decorator:
+	@windows_noexpand_args
+	@click.command(..).
+	def ..
+	"""
+	def __init__(self, cli):
+		self._orig = cli
+	def __call__(self, *args, **kwargs):
+		try:
+			if tuple(map(int, click.__version__.split('.'))) >= (8, 0, 1):
+				kwargs['windows_expand_args'] = False
+		except:
+			import traceback
+			traceback.print_exc(file=sys.stderr)
+		return self._orig(*args, **kwargs)
+	def __getattr__(self, name):
+		return getattr(self._orig, name)
