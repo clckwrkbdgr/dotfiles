ba0393df7b70b88cef2ecadf105f45d3faaba5c1
Dixie Flatline <umi0451@gmail.com>
2021-11-17 22:51:47 +0200

    Special class to represent selected row with header.

--- lib/clckwrkbdgr/sql.py
+++ lib/clckwrkbdgr/sql.py
@@ -3,6 +3,28 @@ try:
 except: # pragma: no cover
 	sqlite3 = None
 
+class Row(object):
+	def __init__(self, values, headers=None):
+		if headers:
+			assert len(values) == len(headers)
+		else:
+			headers = list(map(str, range(len(values))))
+		self._headers = headers
+		self._values = values
+	def get_headers(self):
+		return self._headers
+	def __iter__(self):
+		return iter(self._values)
+	def __getitem__(self, name):
+		try:
+			return self._values[name]
+		except:
+			index = self._headers.index(name)
+			return self._values[index]
+	def __getattr__(self, name):
+		index = self._headers.index(name)
+		return self._values[index]
+
 def execute(database, query): # pragma: no cover -- TODO
 	conn = sqlite3.connect(database)
 	conn.text_factory = str # To prevent some dummy encoding bug.
@@ -24,7 +46,7 @@ def select(database, query): # pragma: no cover -- TODO
 			cursor = conn.cursor()
 			cursor.execute(query)
 			conn.commit()
-			return [row for row in cursor]
+			return [Row(row) for row in cursor]
 		finally:
 			cursor.close()
 	finally:
--- lib/clckwrkbdgr/test/test_sql.py
+++ lib/clckwrkbdgr/test/test_sql.py
@@ -1,1 +1,40 @@
-import clckwrkbdgr.sql
+import unittest
+unittest.defaultTestLoader.testMethodPrefix = 'should'
+from clckwrkbdgr import sql
+
+class TestSqlTableRow(unittest.TestCase):
+	def should_access_row_by_index(self):
+		row = sql.Row([1, 2, 'foo'], ['First', 'Second', 'Third'])
+		self.assertEqual(row[0], 1)
+		self.assertEqual(row[1], 2)
+		self.assertEqual(row[2], 'foo')
+	def should_access_row_by_name(self):
+		row = sql.Row([1, 2, 'foo'], ['First', 'Second', 'Third'])
+		self.assertEqual(row['First'], 1)
+		self.assertEqual(row['Second'], 2)
+		self.assertEqual(row['Third'], 'foo')
+	def should_access_row_by_attr(self):
+		row = sql.Row([1, 2, 'foo'], ['First', 'Second', 'Third'])
+		self.assertEqual(row.First, 1)
+		self.assertEqual(row.Second, 2)
+		self.assertEqual(row.Third, 'foo')
+	def should_iterate_over_row(self):
+		row = sql.Row([1, 2, 'foo'], ['First', 'Second', 'Third'])
+		self.assertEqual(tuple(row), (1, 2, 'foo'))
+	def should_create_sql_row(self):
+		row = sql.Row([1, 2, 'foo'], ['First', 'Second', 'Third'])
+		self.assertEqual(row['First'], 1)
+		self.assertEqual(row['Second'], 2)
+		self.assertEqual(row['Third'], 'foo')
+		self.assertEqual(tuple(row), (1, 2, 'foo'))
+	def should_create_sql_row_without_header(self):
+		row = sql.Row([1, 2, 'foo'])
+		self.assertEqual(row['0'], 1)
+		self.assertEqual(row['1'], 2)
+		self.assertEqual(row['2'], 'foo')
+		self.assertEqual(tuple(row), (1, 2, 'foo'))
+	def should_access_headers(self):
+		row = sql.Row([1, 2, 'foo'], ['First', 'Second', 'Third'])
+		self.assertEqual(row.get_headers(), ['First', 'Second', 'Third'])
+		row = sql.Row([1, 2, 'foo'])
+		self.assertEqual(row.get_headers(), ['0', '1', '2'])
