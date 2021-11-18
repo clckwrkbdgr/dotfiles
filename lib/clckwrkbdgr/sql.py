62e3a9d0de117bb3e0e1a1ed084a7e7490c4616a
Dixie Flatline <umi0451@gmail.com>
2021-11-18 14:07:18 +0200

    Fixed for Py2.

--- lib/clckwrkbdgr/sql.py
+++ lib/clckwrkbdgr/sql.py
@@ -2,15 +2,27 @@ try:
 	import sqlite3
 except: # pragma: no cover
 	sqlite3 = None
+import functools
 
+@functools.total_ordering
 class Row(object):
 	def __init__(self, values, headers=None):
 		if headers:
 			assert len(values) == len(headers)
 		else:
-			headers = list(map(str, range(len(values))))
-		self._headers = headers
-		self._values = values
+			headers = map(str, range(len(values)))
+		self._headers = tuple(headers)
+		self._values = tuple(values)
+	def __str__(self):
+		return '{' + ', '.join('{0}:{1}'.format(repr(header), repr(value)) for header, value in zip(self._headers, self._values)) + '}'
+	def __repr__(self):
+		return '{0}({1}, {2})'.format(self.__class__.__name__, repr(self._values), repr(self._headers))
+	def __eq__(self, other):
+		return self._values == tuple(other)
+	def __ne__(self, other):
+		return self._values != tuple(other)
+	def __lt__(self, other):
+		return self._values < tuple(other)
 	def get_headers(self):
 		return self._headers
 	def __iter__(self):
@@ -46,7 +58,8 @@ def select(database, query): # pragma: no cover -- TODO
 			cursor = conn.cursor()
 			cursor.execute(query)
 			conn.commit()
-			return [Row(row) for row in cursor]
+			headers = tuple(_[0] for _ in cursor.description)
+			return [Row(row, headers) for row in cursor]
 		finally:
 			cursor.close()
 	finally:
--- lib/clckwrkbdgr/test/test_sql.py
+++ lib/clckwrkbdgr/test/test_sql.py
@@ -3,6 +3,23 @@ unittest.defaultTestLoader.testMethodPrefix = 'should'
 from clckwrkbdgr import sql
 
 class TestSqlTableRow(unittest.TestCase):
+	def should_representt_row_as_string(self):
+		row = sql.Row([1, 2, 'foo'], ['First', 'Second', 'Third'])
+		self.assertEqual(str(row), "{'First':1, 'Second':2, 'Third':'foo'}")
+		self.assertEqual(repr(row), "Row((1, 2, 'foo'), ('First', 'Second', 'Third'))")
+	def should_compare_rows_as_tuples_of_Values(self):
+		row = sql.Row([1, 2, 'foo'], ['First', 'Second', 'Third'])
+		same = sql.Row([1, 2, 'foo'], ['First', 'Second', 'Third'])
+		other = sql.Row([1, 3, 'foo'], ['First', 'Second', 'Third'])
+		self.assertEqual(row, same)
+		self.assertNotEqual(row, other)
+		self.assertTrue(row == same)
+		self.assertFalse(row != same)
+		self.assertFalse(row == other)
+		self.assertTrue(row <= same)
+		self.assertFalse(row < same)
+		self.assertTrue(row <= other)
+		self.assertTrue(row < other)
 	def should_access_row_by_index(self):
 		row = sql.Row([1, 2, 'foo'], ['First', 'Second', 'Third'])
 		self.assertEqual(row[0], 1)
@@ -35,6 +52,6 @@ class TestSqlTableRow(unittest.TestCase):
 		self.assertEqual(tuple(row), (1, 2, 'foo'))
 	def should_access_headers(self):
 		row = sql.Row([1, 2, 'foo'], ['First', 'Second', 'Third'])
-		self.assertEqual(row.get_headers(), ['First', 'Second', 'Third'])
+		self.assertEqual(row.get_headers(), ('First', 'Second', 'Third'))
 		row = sql.Row([1, 2, 'foo'])
-		self.assertEqual(row.get_headers(), ['0', '1', '2'])
+		self.assertEqual(row.get_headers(), ('0', '1', '2'))
