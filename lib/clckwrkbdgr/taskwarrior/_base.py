a07cda27d9a0efcc46af03b516ba38efb1861edd
Dixie Flatline <umi0451@gmail.com>
2021-11-22 15:00:22 +0200

    Custom config (taskfile, separator). Fixed several issues with reading/writing task files, including existing legacy ones.

--- lib/clckwrkbdgr/taskwarrior/_base.py
+++ lib/clckwrkbdgr/taskwarrior/_base.py
@@ -1,14 +1,31 @@
 import os, subprocess
 import datetime
 from collections import namedtuple
+try:
+	from pathlib2 import Path
+except: # pragma: no cover
+	from pathlib import Path
 from clckwrkbdgr import xdg
+
+class Config:
+	def __init__(self, taskfile=None, separator=None):
+		self.taskfile = Path(taskfile or xdg.save_data_path('taskwarrior')/'tasklist.lst')
+		self.separator = separator or ' '
 
 class TaskWarrior:
 	Entry = namedtuple('Entry', 'datetime title')
 
-	def __init__(self):
-		self._taskfile = xdg.save_data_path('taskwarrior')/'tasklist.lst'
-	def start(self, task=None):
+	def __init__(self, config=None):
+		self.config = config or Config()
+	def _append(self, task_datetime, task_title):
+		with self.config.taskfile.open('a+') as f:
+			line = task_datetime.isoformat() + self.config.separator + str(task_title).replace('\n', ' ') + '\n'
+			try:
+				f.write(line)
+			except UnicodeError: # pragma: no cover
+				f.write(line.encode('ascii', 'replace').decode())
+	def start(self, task=None, now=None):
+		now = now or datetime.datetime.now()
 		if not task:
 			task = None
 			for current in self.get_history():
@@ -16,11 +33,10 @@ class TaskWarrior:
 					task = current.title
 			if task is None:
 				return False
-		with self._taskfile.open('a+') as f:
-			f.write(datetime.datetime.now().isoformat() + ' ' + str(task).replace('\n', ' ') + '\n')
+		self._append(now, task)
 		return True
 	def stop(self):
-		with self._taskfile.open('a+') as f:
+		with self.config.taskfile.open('a+') as f:
 			f.write(datetime.datetime.now().isoformat() + '\n')
 		return True
 	def get_current_task(self):
@@ -32,17 +48,25 @@ class TaskWarrior:
 			return None
 		return current.title
 	def get_history(self):
-		if not self._taskfile.exists():
+		if not self.config.taskfile.exists():
 			return
-		with self._taskfile.open('r') as f:
+		with self.config.taskfile.open('r') as f:
 			for line in f.readlines():
-				if ' ' in line:
+				if self.config.separator in line:
+					entry_datetime, entry_title = line.rstrip('\n').split(self.config.separator, 1)
+				elif '\t' in line:
+					entry_datetime, entry_title = line.rstrip('\n').split('\t', 1)
+				elif ' ' in line:
 					entry_datetime, entry_title = line.rstrip('\n').split(' ', 1)
 				else:
 					entry_datetime, entry_title = line.rstrip('\n'), None
+				try:
+					entry_datetime = datetime.datetime.strptime(entry_datetime, "%Y-%m-%dT%H:%M:%S.%f")
+				except ValueError:
+					entry_datetime = datetime.datetime.strptime(entry_datetime, "%Y-%m-%dT%H:%M:%S")
 				yield self.Entry(
-						datetime.datetime.strptime(entry_datetime, "%Y-%m-%dT%H:%M:%S.%f"),
+						entry_datetime,
 						entry_title,
 						)
 	def fix_history(self):
-		return 0 == subprocess.call([os.environ.get('EDITOR', 'vi'), str(self._taskfile)])
+		return 0 == subprocess.call([os.environ.get('EDITOR', 'vi'), str(self.config.taskfile)])
--- lib/clckwrkbdgr/taskwarrior/test/test_taskwarrior.py
+++ lib/clckwrkbdgr/taskwarrior/test/test_taskwarrior.py
@@ -8,10 +8,12 @@ mock.patch.TEST_PREFIX = 'should'
 from pyfakefs import fake_filesystem_unittest
 
 import os
+import datetime
+import six
 from clckwrkbdgr import xdg
 import clckwrkbdgr.taskwarrior
 import clckwrkbdgr.taskwarrior._base
-from clckwrkbdgr.taskwarrior import TaskWarrior
+from clckwrkbdgr.taskwarrior import Config, TaskWarrior
 
 class TestTaskWarrior(fake_filesystem_unittest.TestCase):
 	def setUp(self):
@@ -55,10 +57,34 @@ class TestTaskWarrior(fake_filesystem_unittest.TestCase):
 		task.start('bar')
 		task.stop()
 		self.assertEqual([_.title for _ in task.get_history()], ['foo', 'bar', None])
+	def should_use_custom_separator_for_entry_tuple(self):
+		task = TaskWarrior(Config(separator='__'))
+		task.start('foo')
+		task.start('bar')
+		self.assertTrue('__foo' in task.config.taskfile.read_text())
+		task.stop()
+		self.assertEqual([_.title for _ in task.get_history()], ['foo', 'bar', None])
+	def should_try_to_guess_separator_for_line(self):
+		task = TaskWarrior(Config(separator='__'))
+		task.config.taskfile.write_text(six.text_type('\n'.join([
+			'{0}__foo'.format(datetime.datetime(2021, 12, 31, 8, 0, 0).isoformat()),
+			'{0}\tbar with space'.format(datetime.datetime(2021, 12, 31, 8, 10, 0).isoformat()),
+			'{0} baz with space'.format(datetime.datetime(2021, 12, 31, 8, 20, 0).isoformat()),
+			'{0}'.format(datetime.datetime(2021, 12, 31, 8, 30, 0).isoformat()),
+			]) + '\n'))
+		self.assertEqual([_.title for _ in task.get_history()], ['foo', 'bar with space', 'baz with space', None])
+	def should_recognize_isodate_without_msecs(self):
+		task = TaskWarrior(Config(separator='__'))
+		task.config.taskfile.write_text(six.text_type('\n'.join([
+			'{0}\tfoo'.format(datetime.datetime(2021, 12, 31, 8, 0, 0).isoformat().split('.', 1)[0]),
+			'{0}\tbar'.format(datetime.datetime(2021, 12, 31, 8, 10, 0).isoformat()),
+			'{0}'.format(datetime.datetime(2021, 12, 31, 8, 30, 0).isoformat()),
+			]) + '\n'))
+		self.assertEqual([_.title for _ in task.get_history()], ['foo', 'bar', None])
 	@mock.patch('subprocess.call', side_effect=[0])
 	def should_fix_task_history(self, subprocess_call):
 		task = TaskWarrior()
 		self.assertTrue(task.fix_history())
 		subprocess_call.assert_has_calls([
-			mock.call([os.environ.get('EDITOR', 'vi'), str(task._taskfile)]),
+			mock.call([os.environ.get('EDITOR', 'vi'), str(task.config.taskfile)]),
 			])
