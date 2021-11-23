3e5626a900cce673668dc70d49d7b8e01d40da47
Dixie Flatline <umi0451@gmail.com>
2021-11-23 12:16:37 +0200

    Custom aliases for stop/resume actions to keep backwards compatibility with existing task files.

--- lib/clckwrkbdgr/taskwarrior/_base.py
+++ lib/clckwrkbdgr/taskwarrior/_base.py
@@ -1,5 +1,6 @@
 import os, subprocess
 import datetime
+import functools
 from collections import namedtuple
 try:
 	from pathlib2 import Path
@@ -8,12 +9,55 @@ except: # pragma: no cover
 from clckwrkbdgr import xdg
 
 class Config:
-	def __init__(self, taskfile=None, separator=None):
+	def __init__(self, taskfile=None, separator=None,
+			stop_alias=None, resume_alias=None,
+			):
+		""" Configuration for task tracker.
+		- taskfile: storage file for task history.
+		  Default is $XDG_DATA_HOME/taskwarrior/tasklist.lst
+		- separator: text separator between date/time field and task title in the task file.
+		  Default is space.
+		- stop_alias: Task title to put instead of default 'stop' action.
+		  By default 'stop' entries will have title=None.
+		- resume_alias: Task title to put instead of default 'resume' action.
+		  By default 'resume' entries will adopt the title of the last stopped task.
+		"""
 		self.taskfile = Path(taskfile or xdg.save_data_path('taskwarrior')/'tasklist.lst')
 		self.separator = separator or ' '
+		self.stop_alias = stop_alias
+		self.resume_alias = resume_alias
+
+@functools.total_ordering
+class Entry(object):
+	def __init__(self, datetime, title, is_resume=False, is_stop=False):
+		self.datetime = datetime
+		self.title = title
+		assert not (is_resume and is_stop)
+		self.is_resume = is_resume
+		self.is_stop = is_stop
+	def __str__(self):
+		return '{0} {1}'.format(self.datetime, self.title)
+	def __repr__(self):
+		args = [repr(self.datetime), repr(self.title)]
+		if self.is_resume:
+			args.append('is_resume={0}'.format(self.is_resume))
+		if self.is_stop:
+			args.append('is_stop={0}'.format(self.is_stop))
+		return '{0}({1})'.format('Entry', ', '.join(args))
+	def __iter__(self):
+		return iter((self.datetime, self.title))
+	def __lt__(self, other):
+		return (self.datetime, self.title) < (other.datetime, other.title)
+	def __eq__(self, other):
+		return (self.datetime, self.title) == (other.datetime, other.title)
+	def __ne__(self, other):
+		return (self.datetime, self.title) != (other.datetime, other.title)
 
 class TaskWarrior:
-	Entry = namedtuple('Entry', 'datetime title')
+	""" Base interface for Task tracker.
+	Current implementation uses plain text file storage.
+	"""
+	Entry = Entry
 
 	def __init__(self, config=None):
 		self.config = config or Config()
@@ -25,6 +69,9 @@ class TaskWarrior:
 			except UnicodeError: # pragma: no cover
 				f.write(line.encode('ascii', 'replace').decode())
 	def start(self, task=None, now=None):
+		""" Starts new task.
+		If task is None, tries to resume previously stopped task. If there were no tasks yet, does nothing and returns False.
+		"""
 		now = now or datetime.datetime.now()
 		if not task:
 			task = None
@@ -35,21 +82,33 @@ class TaskWarrior:
 				return False
 		self._append(now, task)
 		return True
-	def stop(self):
+	def stop(self, now=None):
+		""" Stops current task. """
+		now = now or datetime.datetime.now()
 		with self.config.taskfile.open('a+') as f:
-			f.write(datetime.datetime.now().isoformat() + '\n')
+			f.write(now.isoformat() + '\n')
 		return True
 	def get_current_task(self):
+		""" Returns title of the current task.
+		Returns None if there were not tasks or the last entry was 'stop'.
+		"""
+		current = self.get_current_entry()
+		if current is None:
+			return None
+		return current.title
+	def get_current_entry(self):
+		""" Returns current task entry or None if there were not tasks yet. """
 		history = iter(self.get_history())
 		current = next(history, None)
 		for current in history:
 			continue
-		if current is None:
-			return None
-		return current.title
-	def get_history(self):
+		return current
+	def get_history(self, entry_class=None):
+		""" Returns full task history in form of Entry objects. """
 		if not self.config.taskfile.exists():
 			return
+		entry_class = entry_class or self.Entry
+		last_started_task_title = '<unknown>'
 		with self.config.taskfile.open('r') as f:
 			for line in f.readlines():
 				if self.config.separator in line:
@@ -64,9 +123,33 @@ class TaskWarrior:
 					entry_datetime = datetime.datetime.strptime(entry_datetime, "%Y-%m-%dT%H:%M:%S.%f")
 				except ValueError:
 					entry_datetime = datetime.datetime.strptime(entry_datetime, "%Y-%m-%dT%H:%M:%S")
-				yield self.Entry(
+
+				entry = entry_class(
 						entry_datetime,
 						entry_title,
 						)
+
+				if entry_title is None:
+					entry.is_stop = True
+				elif self.config.stop_alias and entry_title == self.config.stop_alias:
+					entry.title = None
+					entry.is_stop = True
+				elif self.config.resume_alias and entry_title == self.config.resume_alias:
+					entry.title = last_started_task_title
+					entry.is_resume = True
+				elif entry_title == last_started_task_title:
+					entry.is_resume = True
+
+				if not entry_title:
+					pass
+				elif self.config.resume_alias and entry_title == self.config.resume_alias:
+					pass
+				elif self.config.stop_alias and entry_title == self.config.stop_alias:
+					pass
+				else:
+					last_started_task_title = entry_title
+
+				yield entry
 	def fix_history(self):
+		""" Allows to edit task history manually using text editor. """
 		return 0 == subprocess.call([os.environ.get('EDITOR', 'vi'), str(self.config.taskfile)])
--- lib/clckwrkbdgr/taskwarrior/test/test_taskwarrior.py
+++ lib/clckwrkbdgr/taskwarrior/test/test_taskwarrior.py
@@ -13,7 +13,28 @@ import six
 from clckwrkbdgr import xdg
 import clckwrkbdgr.taskwarrior
 import clckwrkbdgr.taskwarrior._base
-from clckwrkbdgr.taskwarrior import Config, TaskWarrior
+from clckwrkbdgr.taskwarrior import Config, Entry, TaskWarrior
+
+class TestTaskEntry(unittest.TestCase):
+	def should_compare_entries(self):
+		self.assertTrue(Entry(1, 'bar') < Entry(1, 'baz'))
+		self.assertTrue(Entry(1, 'baz') > Entry(1, 'bar'))
+		self.assertTrue(Entry(1, 'bar') < Entry(2, 'bar'))
+		self.assertEqual(Entry(1, 'foo'), Entry(1, 'foo'))
+		self.assertNotEqual(Entry(1, 'bar'), Entry(2, 'bar'))
+		self.assertFalse(Entry(1, 'bar') == Entry(2, 'bar'))
+		self.assertTrue(Entry(1, 'bar') != Entry(2, 'bar'))
+	def should_iter_over_fields(self):
+		self.assertEqual(list(Entry(1, 'bar')), [1, 'bar'])
+	def should_repr_entries(self):
+		self.assertEqual(str(Entry(1, 'bar')), '1 bar')
+		self.assertEqual(repr(Entry(1, 'bar')), 'Entry(1, {0})'.format(repr('bar')))
+		entry = Entry(1, 'bar')
+		entry.is_resume = True
+		self.assertEqual(repr(entry), 'Entry(1, {0}, is_resume=True)'.format(repr('bar')))
+		entry = Entry(1, 'bar')
+		entry.is_stop = True
+		self.assertEqual(repr(entry), 'Entry(1, {0}, is_stop=True)'.format(repr('bar')))
 
 class TestTaskWarrior(fake_filesystem_unittest.TestCase):
 	def setUp(self):
@@ -57,6 +78,72 @@ class TestTaskWarrior(fake_filesystem_unittest.TestCase):
 		task.start('bar')
 		task.stop()
 		self.assertEqual([_.title for _ in task.get_history()], ['foo', 'bar', None])
+	def should_use_custom_aliases_for_stop_and_resume(self):
+		task = TaskWarrior(Config(
+			resume_alias='UNLOCK',
+			stop_alias='LOCK',
+			))
+
+		task.config.taskfile.write_text(six.text_type('\n'.join([
+			'{0} foo'.format(datetime.datetime(2021, 12, 31, 8, 0, 0).isoformat()),
+			'{0} LOCK'.format(datetime.datetime(2021, 12, 31, 8, 10, 0).isoformat()),
+			'{0} UNLOCK'.format(datetime.datetime(2021, 12, 31, 8, 30, 0).isoformat()),
+			]) + '\n'))
+		history = list(task.get_history())
+		self.assertEqual(len(history), 3)
+		self.assertFalse(history[0].is_resume)
+		self.assertFalse(history[0].is_stop)
+		self.assertFalse(history[1].is_resume)
+		self.assertTrue(history[1].is_stop)
+		self.assertTrue(history[2].is_resume)
+		self.assertFalse(history[2].is_stop)
+		self.assertEqual(task.get_current_task(), 'foo')
+
+		task.config.taskfile.write_text(six.text_type('\n'.join([
+			'{0} foo'.format(datetime.datetime(2021, 12, 31, 8, 0, 0).isoformat()),
+			'{0} LOCK'.format(datetime.datetime(2021, 12, 31, 8, 10, 0).isoformat()),
+			'{0} UNLOCK'.format(datetime.datetime(2021, 12, 31, 8, 30, 0).isoformat()),
+			'{0}'.format(datetime.datetime(2021, 12, 31, 8, 0, 0).isoformat()),
+			'{0} foo'.format(datetime.datetime(2021, 12, 31, 8, 0, 0).isoformat()),
+			]) + '\n'))
+		history = list(task.get_history())
+		self.assertEqual(len(history), 5)
+		self.assertFalse(history[0].is_resume)
+		self.assertFalse(history[0].is_stop)
+		self.assertFalse(history[1].is_resume)
+		self.assertTrue(history[1].is_stop)
+		self.assertTrue(history[2].is_resume)
+		self.assertFalse(history[2].is_stop)
+		self.assertFalse(history[3].is_resume)
+		self.assertTrue(history[3].is_stop)
+		self.assertTrue(history[4].is_resume)
+		self.assertFalse(history[4].is_stop)
+		self.assertEqual(task.get_current_task(), 'foo')
+
+		task.config.taskfile.write_text(six.text_type('\n'.join([
+			'{0} foo'.format(datetime.datetime(2021, 12, 31, 8, 0, 0).isoformat()),
+			'{0} LOCK'.format(datetime.datetime(2021, 12, 31, 8, 10, 0).isoformat()),
+			'{0} UNLOCK'.format(datetime.datetime(2021, 12, 31, 8, 30, 0).isoformat()),
+			'{0} bar'.format(datetime.datetime(2021, 12, 31, 8, 40, 0).isoformat()),
+			'{0} foo'.format(datetime.datetime(2021, 12, 31, 8, 50, 0).isoformat()),
+			]) + '\n'))
+		history = list(task.get_history())
+		self.assertEqual(len(history), 5)
+		self.assertFalse(history[0].is_resume)
+		self.assertFalse(history[0].is_stop)
+		self.assertFalse(history[1].is_resume)
+		self.assertTrue(history[1].is_stop)
+		self.assertIsNone(history[1].title)
+		self.assertTrue(history[2].is_resume)
+		self.assertFalse(history[2].is_stop)
+		self.assertEqual(history[2].title, 'foo')
+		self.assertFalse(history[3].is_resume)
+		self.assertFalse(history[3].is_stop)
+		self.assertEqual(history[3].title, 'bar')
+		self.assertFalse(history[4].is_resume)
+		self.assertFalse(history[4].is_stop)
+		self.assertEqual(history[4].title, 'foo')
+		self.assertEqual(task.get_current_task(), 'foo')
 	def should_use_custom_separator_for_entry_tuple(self):
 		task = TaskWarrior(Config(separator='__'))
 		task.start('foo')
