f7d099048a6c19bb6d354bb81913d1e995342b48
Dixie Flatline <umi0451@gmail.com>
2021-12-02 12:13:38 +0200

    Unit tests.

--- lib/clckwrkbdgr/email.py
+++ lib/clckwrkbdgr/email.py
@@ -1,19 +1,25 @@
-import email
+from __future__ import absolute_import
+import email, email.message
+import six
 
-class Message(object): # pragma: no cover -- TODO
+class Message(object):
 	def __init__(self, data):
-		self.data = email.message_from_bytes(data)
+		if isinstance(data, email.message.EmailMessage):
+			self.data = data
+		elif isinstance(data, six.string_types):
+			self.data = email.message_from_string(data)
+		elif isinstance(data, six.binary_type): # pragma: no cover -- py2
+			self.data = email.message_from_bytes(data)
+		else:
+			raise ValueError("Message should be either MailMessage, or str, or bytes, but not {0}".format(type(data)))
 	def get_header(self, header_name):
 		header = self.data[header_name]
 		result = ''
 		for part, encoding in email.header.decode_header(header):
-			if isinstance(part, str):
+			if isinstance(part, six.string_types):
 				result += part
-			else:
-				try:
-					result += part.decode(encoding if encoding else 'ascii')
-				except:
-					result += repr(part)
+			else: # pragma: no cover -- TODO need real case.
+				result += part.decode(encoding if encoding else 'ascii', 'replace')
 		return result
 	def get_full_text(self):
 		return self._decode_payloads(self.data)
@@ -21,10 +27,10 @@ class Message(object): # pragma: no cover -- TODO
 		if not data.is_multipart():
 			body = data.get_payload(decode=True)
 			try:
-				if isinstance(body, bytes):
+				if not isinstance(body, six.string_types): # pragma: no cover -- py2
 					charset = data.get_charset()
 					body = body.decode(charset if charset else 'utf-8')
-			except:
+			except: # pragma: no cover
 				pass
 			return str(body)
 		else:
@@ -32,7 +38,7 @@ class Message(object): # pragma: no cover -- TODO
 			for part in data.get_payload():
 				try:
 					part = str(part)
-				except KeyError as e:
+				except KeyError as e: # pragma: no cover -- TODO need real case.
 					if str(e) == "'content-transfer-encoding'":
 						part['Content-Transfer-Encoding'] = 'quoted-printable'
 						part = str(part)
--- lib/clckwrkbdgr/test/test_email.py
+++ lib/clckwrkbdgr/test/test_email.py
@@ -1,1 +1,66 @@
-import clckwrkbdgr.email
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+import unittest
+unittest.defaultTestLoader.testMethodPrefix = 'should'
+import clckwrkbdgr.email as E
+import email, email.message, email.mime, email.mime.text, email.mime.multipart
+import six
+try:
+	email.message.EmailMessage
+except AttributeError: # pragma: no cover
+	email.message.EmailMessage = email.message.Message
+
+class TestEmail(unittest.TestCase):
+	def _create(self,
+			subject='Hello world!',
+			plain_body='hello world\n',
+			html_body='<p>hello world</p>\n',
+			multipart=False,
+			):
+		if multipart:
+			msg = email.mime.multipart.MIMEMultipart()
+			msg.attach(email.mime.text.MIMEText(plain_body, 'plain'))
+			msg.attach(email.mime.text.MIMEText(html_body, 'html'))
+		else:
+			msg = email.message.EmailMessage()
+			if six.PY2: # pragma: no cover
+				msg.set_payload(plain_body)
+			else: # pragma: no cover
+				msg.set_content(plain_body)
+		msg['Subject'] = subject
+		msg['From'] = 'JC Denton'
+		msg['To'] = 'Paul Denton'
+		return msg
+	def should_construct_email_from_bytes(self):
+		msg = self._create()
+		if six.PY2: # pragma: no cover
+			msg = msg.as_string()
+		else: # pragma: no cover
+			msg = msg.as_bytes()
+		msg = E.Message(msg)
+		self.assertEqual(msg.get_header('Subject'), 'Hello world!')
+		self.assertEqual(msg.get_full_text(), 'hello world\n')
+	def should_construct_email_from_string(self):
+		msg = E.Message(self._create().as_string())
+		self.assertEqual(msg.get_header('Subject'), 'Hello world!')
+		self.assertEqual(msg.get_full_text(), 'hello world\n')
+	def should_construct_email_from_object(self):
+		msg = E.Message(self._create())
+		self.assertEqual(msg.get_header('Subject'), 'Hello world!')
+		self.assertEqual(msg.get_full_text(), 'hello world\n')
+	def should_not_construct_email_from_anything_else(self):
+		with self.assertRaises(ValueError):
+			msg = E.Message(dict())
+	def should_decode_headers(self):
+		subject = 'Привет Мир!'
+		msg = E.Message(self._create(
+			subject=subject,
+			))
+		self.assertEqual(msg.get_header('Subject'), subject)
+		self.assertEqual(msg.get_full_text(), 'hello world\n')
+	def should_parse_multipart_message(self):
+		msg = E.Message(self._create(
+			multipart=True,
+			).as_string())
+		self.assertEqual(msg.get_header('Subject'), 'Hello world!')
+		self.assertEqual(msg.get_full_text(), 'hello world\n<p>hello world</p>\n')
