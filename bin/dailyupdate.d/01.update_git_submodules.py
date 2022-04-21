85a2b6ffb7231f51d54696dd84c33abf462bac9f
Dixie Flatline <umi0451@gmail.com>
2022-04-21 10:17:23 +0300

    Update submodules only on a single dedicated host that has Git installed.

--- bin/dailyupdate.d/01.update_git_submodules.py
+++ bin/dailyupdate.d/01.update_git_submodules.py
@@ -13,5 +13,12 @@ if userstate.get_flag('metered_network'):
 import clckwrkbdgr.vcs.git as git
 if not clckwrkbdgr.vcs.git.is_repo_root():
 	context.done()
+
+import clckwrkbdgr.pkg
+required_caps = 'git'
+current_caps = 'git' if clckwrkbdgr.pkg.is_app_installed("git") else 'no-git'
+if not context.validate_worker_host('dotfiles.submodules', caps_type=str, required_caps=required_caps, current_caps=current_caps):
+	context.done()
+
 context | git.update_submodules()
 context.done()
