31996c01c9a1d671a924d725a4f2546ea2cd0d38
Dixie Flatline <umi0451@gmail.com>
2022-06-19 08:27:08 +0300

    Directory .local/share was processed twice.

--- setup.py
+++ setup.py
@@ -5,17 +5,18 @@ Performs setup of dotfiles and some system options.
 See '--help' for usage.
 """
 import os
+from collections import OrderedDict
 import click
 import clckwrkbdgr.jobsequence as JB
 
 setup = JB.JobSequence(
 		verbose_var_name='DOTFILES_SETUP_VERBOSE',
-		default_job_dir=[
+		default_job_dir=list(OrderedDict.fromkeys((
 			os.path.join(os.environ['XDG_CONFIG_HOME'], 'setup.d'),
 			os.path.join(os.environ['XDG_DATA_HOME'], 'setup.d'),
 			os.path.join(os.environ['HOME'], '.local', 'setup.d'),
 			os.path.join(os.environ['HOME'], '.local', 'share', 'setup.d'),
-			],
+			))),
 		click=click,
 		)
 
