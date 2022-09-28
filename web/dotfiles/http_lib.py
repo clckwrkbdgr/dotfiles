d908f51d84cec43bc3d98451fec8f086d03a3646
Dixie Flatline <umi0451@gmail.com>
2022-09-28 15:17:53 +0300

    New Firefox 100+ ESR does not store localstorage in accessible way, so introduced intermediate POST request (mostly for userscripts) for badger web server to store requested localstorage values in FS in plain JSON.

--- web/dotfiles/http_lib.py
+++ web/dotfiles/http_lib.py
@@ -1,3 +1,4 @@
+import json
 import bottle
 from pathlib import Path
 from clckwrkbdgr import xdg
@@ -10,6 +11,24 @@ def host_dotfiles_js_library(javascript_module):
 	bottle.response.content_type = 'application/javascript'
 	return (DOTFILES_ROOTDIR/'lib'/'{0}.js'.format(javascript_module)).read_text()
 
+@bottle.post('/lib/save_to_fs_storage')
+def save_to_fs_storage():
+	data = bottle.request.body.read().decode('utf-8', 'replace')
+	data = json.loads(data)
+	domain = data['domain']
+	name = data['name']
+	value = data['value']
+
+	local_path = xdg.save_state_path('firefox')/'localstorage'
+	local_path.mkdir(parents=True, exist_ok=True)
+
+	filename = local_path/'{0}.json'.format(domain)
+	data = {}
+	if filename.exists():
+		data = json.loads(filename.read_text())
+	data[name] = value
+	filename.write_text(json.dumps(data))
+
 @bottle.route('/lib/test/test_<javascript_module>.html')
 def host_dotfiles_js_library(javascript_module):
 	return (DOTFILES_ROOTDIR/'lib'/'test'/'test_{0}.html'.format(javascript_module)).read_text()
@@ -21,4 +40,10 @@ def post_test_results(javascript_module):
 @bottle.route('/lib/test/userscript')
 @bottle.route('/lib/test/userscript_with_grant')
 def test_userscripts():
-	return (DOTFILES_ROOTDIR/'test_userscript.html').read_text()
+	return (DOTFILES_ROOTDIR/'web'/'dotfiles'/'test_userscript.html').read_text()
+
+@bottle.post('/lib/test/save_to_fs_storage')
+def test_save_to_fs_storage():
+	data = bottle.request.body.read().decode('utf-8', 'replace')
+	data = json.loads(data)
+	print(data)
