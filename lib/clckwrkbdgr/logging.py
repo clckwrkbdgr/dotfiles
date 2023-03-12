7749682737d106b2d390b7d6a3180fe3f04be770
Dixie Flatline <umi0451@gmail.com>
2023-03-12 15:42:02 +0200

    Custom logging utilities.

--- lib/clckwrkbdgr/logging.py
+++ lib/clckwrkbdgr/logging.py
@@ -1,7 +1,73 @@
 from __future__ import absolute_import
+import sys
 import logging
+import vintage
 
+@vintage.deprecated('Use clckwrkbdgr.logging.init() instead.')
 def getFileLogger(name, filename): # pragma: no cover
-	logger = logging.getLogger(name)
-	logger.addHandler(logging.FileHandler(str(filename), delay=True))
+	return init(name, filename=filename, stream=None)
+
+def init(logger,
+		debug=False, verbose=False,
+		filename=None, stream=sys.stderr,
+		):
+	""" Creates and initializes specified logger (logging.Logger object or name).
+
+	If verbose is True, sets INFO level. Default level is WARNING.
+	If debug is True, sets DEBUG level. Parameter verbose is ignored in this case.
+
+	By default writes to stderr. Stream could be specified or set to None.
+	If filename is given, duplicates output to the given file.
+
+	Uses following formats:
+	- stream: [<LEVEL>] <logger name>:<message>
+	- file: <time>:<logger name>:<LEVEL>:<message>
+
+	Returns created logger. Logger can also be accessed later using logging.getLogger(name)
+	Returned logger will be callable, direct call will be equivalent to calling logging function corresponding to minimal logging level (e.g. logger.debug, logger.warning etc).
+
+	Also, if root logger wasn't initialized, calls logging.basicConfig with the same parameters.
+	"""
+	if isinstance(logger, str):
+		logger = logging.getLogger(logger)
+
+	if logger.handlers:
+		for _handler in logger.handlers[:]:
+			logger.removeHandler(_handler)
+	logger.propagate = False
+
+	level = logging.WARNING
+	if debug:
+		level = logging.DEBUG
+	elif verbose:
+		level = logging.INFO
+
+	loggers = [logger]
+	if not logging.getLogger().handlers:
+		logging.basicConfig()
+		for _handler in logging.root.handlers[:]: # We will use custom logger instead of the stock one.
+			logging.root.removeHandler(_handler)
+		loggers.append(logging.getLogger())
+
+	for logger_obj in loggers:
+		if stream:
+			stream_handler = logging.StreamHandler(stream)
+			stream_handler.setFormatter(logging.Formatter(
+				'[%(levelname)s] %(name)s: %(message)s',
+				))
+			logger_obj.addHandler(stream_handler)
+			# TODO colors
+			# TODO DEBUG logging.basicConfig(format='%(module)s:%(lineno)d:%(funcName)s:%(levelname)s:%(message)s')
+			# TODO DEBUG _handler.setFormatter(logging.Formatter('%(process)d: %(asctime)s: %(module)s:%(lineno)d:%(funcName)s: [%(levelname)s] %(message)s'))
+		if filename:
+			file_handler = logging.FileHandler(str(filename), delay=True)
+			file_handler.setFormatter(logging.Formatter(
+				'%(asctime)s:%(name)s:%(levelname)s: %(message)s',
+				datefmt='%Y-%m-%d %H:%M:%S',
+				))
+			logger_obj.addHandler(file_handler)
+		logger_obj.setLevel(level)
+
+	logger.__call__ = lambda *args, **kwargs: logger.log(level, *args, **kwargs)
+
 	return logger
--- lib/clckwrkbdgr/test/test_logging.py
+++ lib/clckwrkbdgr/test/test_logging.py
@@ -1,2 +1,71 @@
+import time
+import io
+import logging
+try:
+	from pathlib2 import Path
+except: # pragma: no cover
+	from pathlib import Path
 from clckwrkbdgr import unittest
 import clckwrkbdgr.logging
+from clckwrkbdgr.logging import init
+
+class TestBasicLogger(unittest.TestCase):
+	def should_print_message_to_stream(self):
+		with io.StringIO() as stream:
+			logger = init('should_print_message_to_stream', stream=stream)
+			logger.warning(u'Hello world!')
+			logger.info(u'Info.')
+			logger.debug(u'Debug...')
+			self.assertEqual(stream.getvalue(), '[WARNING] should_print_message_to_stream: Hello world!\n')
+	def should_print_message_to_stream_using_root_logger(self):
+		for _handler in logging.root.handlers[:]: # pragma: no cover
+			logging.root.removeHandler(_handler)
+		with io.StringIO() as stream:
+			logger = init('should_print_message_to_stream_using_root_logger', stream=stream)
+			logging.warning(u'I am root!')
+			self.assertEqual(stream.getvalue(), '[WARNING] root: I am root!\n')
+	def should_reinit_logger(self):
+		with io.StringIO() as stream:
+			logger = init('should_reinit_logger', stream=stream)
+			logger.warning(u'Reinit logger!')
+			logger = init('should_reinit_logger', stream=stream)
+			logger.warning(u'Reinit logger!')
+			self.assertEqual(stream.getvalue(), '[WARNING] should_reinit_logger: Reinit logger!\n' * 2)
+	def should_create_verbose_logger(self):
+		with io.StringIO() as stream:
+			logger = init('should_create_verbose_logger', verbose=True, stream=stream)
+			logger.warning(u'Warning!')
+			logger.info(u'Info.')
+			logger.debug(u'Debug...')
+			self.assertEqual(stream.getvalue(),
+					'[WARNING] should_create_verbose_logger: Warning!\n'
+					'[INFO] should_create_verbose_logger: Info.\n'
+					)
+	def should_create_debug_logger(self):
+		with io.StringIO() as stream:
+			logger = init('should_create_debug_logger', verbose=False, debug=True, stream=stream)
+			logger.warning(u'Warning!')
+			logger.info(u'Info.')
+			logger.debug(u'Debug...')
+			self.assertEqual(stream.getvalue(),
+					'[WARNING] should_create_debug_logger: Warning!\n'
+					'[INFO] should_create_debug_logger: Info.\n'
+					'[DEBUG] should_create_debug_logger: Debug...\n'
+					)
+
+class TestFileLogger(unittest.fs.TestCase):
+	MODULES = [clckwrkbdgr.logging]
+	@unittest.mock.patch('logging.Formatter.converter', side_effect=[time.gmtime(10000)])
+	def should_print_message_to_file_and_stream(self, time_localtime):
+		logfile = Path('/trace.log')
+		with io.StringIO() as stream:
+			logger = init('should_print_message_to_file_and_stream', stream=stream, filename=logfile)
+			logger.warning(u'File and stream!')
+			self.assertEqual(stream.getvalue(), '[WARNING] should_print_message_to_file_and_stream: File and stream!\n')
+		self.assertEqual(logfile.read_text(), '1970-01-01 02:46:40:should_print_message_to_file_and_stream:WARNING: File and stream!\n')
+	@unittest.mock.patch('logging.Formatter.converter', side_effect=[time.gmtime(10000)])
+	def should_print_message_to_file_only(self, time_localtime):
+		logfile = Path('/trace.log')
+		logger = init('should_print_message_to_file_and_stream', stream=None, filename=logfile)
+		logger.warning(u'File only!')
+		self.assertEqual(logfile.read_text(), '1970-01-01 02:46:40:should_print_message_to_file_and_stream:WARNING: File only!\n')
