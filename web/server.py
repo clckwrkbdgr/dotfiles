8f6f55567bac9593adf39e04debf2b25995f42ae
Dixie Flatline <umi0451@gmail.com>
2023-04-28 14:51:56 +0300

    Temp. workaround to prevent flooding of BrokenPipeError.

--- web/server.py
+++ web/server.py
@@ -3,6 +3,15 @@ import blackcompany
 import blackcompany.serve
 import os, sys
 
+# Hiding BrokenPipeError stacks from log.
+# TODO: should be replaced by more robust WSGI server, e.g. CherryPy.
+from wsgiref.handlers import BaseHandler
+import sys
+def ignore_broken_pipes(self): 
+	if sys.exc_info()[0] != BrokenPipeError: BaseHandler.__handle_error_original_(self)
+BaseHandler.__handle_error_original_ = BaseHandler.handle_error
+BaseHandler.handle_error = ignore_broken_pipes
+
 badger_web_root = os.environ.get('BADGER_WEB_ROOT')
 if badger_web_root and os.path.isdir(badger_web_root):
 	blackcompany.serve.plugin.discover(badger_web_root)
