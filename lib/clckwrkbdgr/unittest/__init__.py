35ff234ccab96cb82af9725001aefc630acf33e4
Dixie Flatline <umi0451@gmail.com>
2023-04-30 19:22:09 +0300

    Monkey patch for measuring time taken by test execution in verbose mode.

--- lib/clckwrkbdgr/unittest/__init__.py
+++ lib/clckwrkbdgr/unittest/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 from unittest import *
+from unittest import runner as _runner
 def load_tests(loader, tests, pattern): # pragma: no cover -- fixes loading invalid tests, see doctest:
 	""" For some reason when its `clckwrkbdgr/unittest/__init__.py`
 	instead of `clckwrkbdgr/unittest.py`, unittest discovery finds and loads
@@ -17,6 +18,31 @@ def load_tests(loader, tests, pattern): # pragma: no cover -- fixes loading inva
 	suite.addTests(unittest_tests)
 	return suite
 defaultTestLoader.testMethodPrefix = 'should'
+
+import time
+try:
+	time.perf_counter
+except AttributeError: # pragma: no cover -- py2
+	time.perf_counter = time.time
+
+class TimedTextTestResult(_runner.TextTestResult):
+	def addSuccess(self, test):
+		if self.showAll: # pragma: no cover -- too special conditions, need verbose unit test runner.
+			if not hasattr(self, '_test_stopTime') or self._test_stopTime < self._test_startTime:
+				self._test_stopTime = time.perf_counter()
+			timeTaken = self._test_stopTime - self._test_startTime
+			self.stream.write("({0:.3f}s) ".format(timeTaken))
+		super(TimedTextTestResult, self).addSuccess(test)
+	def startTest(self, test):
+		super(TimedTextTestResult, self).startTest(test)
+		self._test_startTime = time.perf_counter()
+	def stopTest(self, test):
+		super(TimedTextTestResult, self).stopTest(test)
+		self._test_stopTime = time.perf_counter()
+
+class TimedTextTestRunner(_runner.TextTestRunner):
+	resultclass = TimedTextTestResult
+_runner.TextTestRunner = TimedTextTestRunner
 
 try:
 	TestCase.assertCountEqual
