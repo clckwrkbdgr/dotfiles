1292450b673fc08c384339c38e59f214919ce40a
Dixie Flatline <umi0451@gmail.com>
2023-08-07 16:09:41 +0300

    Unit tests for autoexplorer. Fixed platform-specific effects for wave algorithm.

--- lib/clckwrkbdgr/rogue/__main__.py
+++ lib/clckwrkbdgr/rogue/__main__.py
@@ -6,9 +6,9 @@ trace = logging.getLogger('rogue')
 from .. import logging
 from ..fs import SerializedEntity
 from .. import xdg
-from ..math import algorithm
 from ..math import Point, Size, Rect, get_neighbours, sign
 from .dungeon import Dungeon
+from .auto import Autoexplorer
 
 def main(stdscr):
 	logging.init('rogue',
@@ -27,91 +27,6 @@ def main(stdscr):
 	game = Game(dungeon)
 	game.run(stdscr)
 	savefile.save()
-
-class DungeonWave(algorithm.Wave):
-	def __init__(self, matrix, region):
-		self.matrix = matrix
-		self.region = region
-	def is_linked(self, node_from, node_to):
-		distance = abs(node_from - node_to)
-		return distance.x <= 1 and distance.y <= 1
-	def get_links(self, node):
-		return [p for p in get_neighbours(
-			self.matrix, node,
-			with_diagonal=True,
-			check=lambda c: c == '.'
-			)
-			 if self.region.contains(p, with_border=True)
-			 ]
-
-class Autoexplorer:
-	def __init__(self):
-		self.path = None
-		self.previous_direction = None
-	def process(self, dungeon):
-		if self.path:
-			direction = self.path.pop(0)
-			self.previous_direction = direction
-			return direction
-		target = dungeon.rogue
-		for _ in range(100):
-			target = Point(
-					random.randrange(dungeon.rogue.x - 10, dungeon.rogue.x + 10),
-					random.randrange(dungeon.rogue.y - 10, dungeon.rogue.y + 10),
-					)
-			trace.debug('Autoexplorer target: trying {0}'.format(target))
-			if dungeon.terrain.cell(target) != '.':
-				trace.debug('Autoexplorer target: {0}: not passable'.format(target))
-				continue
-			distance = target - dungeon.rogue
-			diff = abs(distance)
-			direction = Point(sign(distance.x), sign(distance.y))
-			trace.debug('Autoexplorer target: selected direction: {0}'.format(direction))
-			if self.previous_direction:
-				trace.debug('Autoexplorer target: previous direction: {0}'.format(self.previous_direction))
-				if self.previous_direction.x == 0:
-					old_directions = [
-							Point(-1, 0),
-							Point(-1, -self.previous_direction.y),
-							Point(0, -self.previous_direction.y),
-							Point(1, -self.previous_direction.y),
-							Point(1, 0),
-							]
-				elif self.previous_direction.y == 0:
-					old_directions = [
-							Point(0, -1),
-							Point(-self.previous_direction.x, -1),
-							Point(-self.previous_direction.x, 0),
-							Point(-self.previous_direction.x, 1),
-							Point(0, 1),
-							]
-				else:
-					old_directions = [
-							Point(self.previous_direction.x, -self.previous_direction.y),
-							Point(0, -self.previous_direction.y),
-							Point(-self.previous_direction.x, -self.previous_direction.y),
-							Point(-self.previous_direction.x, 0),
-							Point(-self.previous_direction.x, self.previous_direction.y),
-							]
-				trace.debug('Autoexplorer target: old direction fan: {0}'.format(old_directions))
-				if direction in old_directions:
-					trace.debug('Autoexplorer target: selected direction is old.')
-					continue
-			if not(3 < diff.x < 10 and 3 < diff.y < 10):
-				trace.debug('Autoexplorer target: too close or too far: {0}'.format(target))
-				continue
-			trace.debug('Autoexplorer target: good, picking {0}'.format(target))
-			break
-		wave = DungeonWave(dungeon.terrain, Rect(
-			topleft=dungeon.rogue - Point(10, 10),
-			size=Size(21, 21),
-			))
-		trace.debug('Autoexplorer forming path: {0} -> {1}'.format(dungeon.rogue, target))
-		path = wave.run(dungeon.rogue, target)
-		trace.debug('Autoexplorer wave: {0}'.format(path))
-		self.path = list(b - a for a, b in zip(path, path[1:]))
-		trace.debug('Autoexplorer sequence: {0}'.format(self.path))
-		return self.process(None)
 
 class Game:
 	CONTROLS = {(ord(k) if isinstance(k, str) else k):v for k,v in {
---
+++ lib/clckwrkbdgr/rogue/test/test_auto.py
@@ -0,0 +1,135 @@
+from ... import logging
+from ... import unittest
+from ..dungeon import Dungeon
+from .. import auto
+
+class MockDungeon(Dungeon):
+	pass
+
+class MockBuilder:
+	def __init__(self, rogue_pos=None, maps=None):
+		self.rogue_pos = rogue_pos or (0, 0)
+		self.maps = maps
+	def build_block(self, block):
+		layout = self.maps.pop(0)
+		for y, line in enumerate(layout.splitlines()):
+			for x, cell in enumerate(line):
+				block.set_cell((x, y), cell)
+	def place_rogue(self, terrain):
+		return self.rogue_pos
+
+EMPTY_MAP = '\n'.join(["." * 32] * 32)
+MAIN_TEST_MAP = unittest.dedent("""\
+		.123456789.123456789.123456789..
+		1..............................1
+		2..............................2
+		3..............................3
+		4..............................4
+		5..............................5
+		6..............................6
+		7..............................7
+		8..............................8
+		9..............................9
+		................................
+		1..............................1
+		2..............................2
+		3..............................3
+		4..............................4
+		5...............#####..........5
+		6...................#..........6
+		7.............#######..........7
+		8...............#...#..........8
+		9.................#.#..........9
+		...............######...........
+		1..............................1
+		2..............................2
+		3..............................3
+		4..............................4
+		5..............................5
+		6..............................6
+		7..............................7
+		8..............................8
+		9..............................9
+		................................
+		.123456789.123456789.123456789..
+		""")
+
+class TestAutoExplorer(unittest.TestCase):
+	@staticmethod
+	def to_string(dungeon): # pragma: no cover
+		result = []
+		for y in range(-20, 20):
+			result.append('')
+			for x in range(-20, 20):
+				result[-1] += dungeon.get_sprite((x, y))
+		return '\n'.join(result) + '\n'
+
+	def setUp(self):
+		builder = MockBuilder(rogue_pos=(10, 10), maps=[
+			EMPTY_MAP, EMPTY_MAP, EMPTY_MAP,
+			EMPTY_MAP, MAIN_TEST_MAP, EMPTY_MAP,
+			EMPTY_MAP, EMPTY_MAP, EMPTY_MAP,
+			])
+		self.dungeon = MockDungeon(builder=builder)
+		self.autoexplore = auto.Autoexplorer()
+
+	def step(self, expected=None):
+		try:
+			result = self.autoexplore.process(self.dungeon)
+			self.dungeon.control(result)
+			if expected:
+				self.assertEqual(result, expected)
+		except: # pragma: no cover
+			print(self.to_string(self.dungeon))
+			raise
+
+	@unittest.mock.patch('random.randrange')
+	def should_generate_random_dungeon(self, random_randrange):
+		random_randrange.side_effect = [
+				15, 15,
+				]
+
+		self.step((1, 1))
+		self.step((1, 1))
+		self.step((1, 1))
+		self.step((1, 1))
+		self.step((1, 1))
+
+		random_randrange.side_effect = [
+				20, 20, # Not passable.
+				16, 16, # Too close.
+				25, 25, # Too far.
+				19, 19,
+				]
+		self.step((-1, 1))
+		self.step((-1, 1))
+		self.step((1, 1))
+		self.step((1, 1))
+		self.step((1, 0))
+		self.step((1, 0))
+		self.step((1, -1))
+	@unittest.mock.patch('random.randrange')
+	def should_not_walk_in_old_directions(self, random_randrange):
+		random_randrange.side_effect = [
+				15, 10,
+				]
+
+		self.step((1, 0))
+		self.step((1, -1))
+		self.step((1, -1))
+		self.step((1, 1))
+		self.step((1, 1))
+
+		random_randrange.side_effect = [
+				5, 10, # Old direction.
+				5, 5, # Old direction.
+				22, 13,
+				26, 18,
+				26, 23,
+				26, 18, # Old direction.
+				26, 28,
+				]
+		for _ in range(7): self.step()
+		for _ in range(5): self.step()
+		for _ in range(5): self.step()
+		for _ in range(5): self.step()
