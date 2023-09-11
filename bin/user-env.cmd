730c4cd11e971549bd3b8d44604a8c625b70d964
Dixie Flatline <umi0451@gmail.com>
2023-09-11 14:31:38 +0300

    User path should be re-imported only once in a shell session.

--- bin/user-env.cmd
+++ bin/user-env.cmd
@@ -9,9 +9,17 @@
    REM While actual users need otherwise
    REM We also cannot use 'normal' parsing functionality that comes with CMD (FOR ...), because it results in recursion somehow.
 
-   REG QUERY HKCU\Environment /V Path | FINDSTR REG_EXPAND_SZ 2>NUL | python.exe -c "import os, sys; filename=os.environ['TEMP'] + '\\userpath.bat'; f = open(filename, 'w'); f.write('@ECHO OFF\nSET \x22USER_PATH='); f.write(sys.stdin.read().split(None, 2)[-1].rstrip()); f.write('\x22')" 2>NUL
-   call %TEMP%\userpath.bat
-   SET "PATH=%USER_PATH%;%PATH%"
+   REM First we check if user path is already prepended: it should be present twice, one for already _appended_ by the idiot MS, and one "fixed" by us.
+   ECHO "%PATH%" | FINDSTR /R "\\.config\\bin\\windows.*\\.config\\bin\\windows" >NUL
+   IF ERRORLEVEL 1 GOTO NO_USER_PATH_YET
+   GOTO USERPATH_IS_ALREADY_SET
+   REM This hack with GOTO is used because if we try to SET varible inside IF ( ) block, it will be expanded (to empty string, ofc) before script is executed at all.
+   :NO_USER_PATH_YET
+      REM And we use Python, because there is no valid and proper option in plain CMD to catch output from a command and store it in a variable.
+      REG QUERY HKCU\Environment /V Path | FINDSTR REG_EXPAND_SZ 2>NUL | python.exe -c "import os, sys; filename=os.environ['TEMP'] + '\\userpath.bat'; f = open(filename, 'w'); f.write('@ECHO OFF\nSET \x22USER_PATH='); f.write(sys.stdin.read().split(None, 2)[-1].rstrip()); f.write('\x22')" 2>NUL
+      call %TEMP%\userpath.bat
+      SET "PATH=%USER_PATH%;%PATH%"
+   :USERPATH_IS_ALREADY_SET
 
    REM Loading generic doskey aliases (if present).
    IF EXIST %USERPROFILE%\.config\alias.doskey (
