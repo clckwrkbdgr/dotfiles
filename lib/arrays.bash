29a7ace300746dfefe8397f9a45f57f3b2bf8556
Dixie Flatline <umi0451@gmail.com>
2024-01-24 15:21:16 +0200

    Escape subshells during appending to array.

--- lib/arrays.bash
+++ lib/arrays.bash
@@ -2,7 +2,7 @@ arrays::append() {
    # Wrapper to add value to array considering different versions of Bash.
    # Params: <array_name> <value>
    local array_name=$1
-   local value=$2
+   local value=${2//\(/'"''"'\(} # Escape '..$(..'=>'..$""(..' to prevent executing subshells during eval below.
    case "$BASH_VERSION" in
       3.*)
          eval "${array_name}[\${#${array_name}[@]}]=\"${value}\""
--- lib/test/test_arrays.bash
+++ lib/test/test_arrays.bash
@@ -1,13 +1,19 @@
 #!/bin/bash
-. "$XDG_CONFIG_HOME/lib/arrays.bash"
-. "$XDG_CONFIG_HOME/lib/unittest.bash"
+. "../lib/arrays.bash"
+. "../lib/unittest.bash"
 
-should_print_usage_info() {
+should_append_values_to_array() {
    declare -a indexed_array
    arrays::append indexed_array 1
    arrays::append indexed_array foo
    assertStringsEqual "${indexed_array[0]}" 1
    assertStringsEqual "${indexed_array[1]}" foo
+}
+
+should_append_values_with_subshells_to_array() {
+   declare -a indexed_array
+   arrays::append indexed_array "foo$""(subshell)"
+   assertStringsEqual "${indexed_array[0]}" "foo$""(subshell)"
 }
 
 unittest::run should_
