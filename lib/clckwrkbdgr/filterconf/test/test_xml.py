5a7c3a523542607f54f91b2696044e0a95ce7e84
Dixie Flatline <umi0451@gmail.com>
2024-03-18 19:29:38 +0200

    Basic XML actions.

---
+++ lib/clckwrkbdgr/filterconf/test/test_xml.py
@@ -0,0 +1,18 @@
+import textwrap
+import six
+from clckwrkbdgr import unittest
+from clckwrkbdgr.filterconf.xml import XMLConfig
+
+class TestXMLConfig(unittest.TestCase):
+	def should_prettify_xml_doc(self):
+		content = '<root><item attr2="foo" attr1="bar"/><item attr="foo">foobar</item></root>'
+		expected = textwrap.dedent("""\
+				<root>
+				  <item attr2="foo" attr1="bar"/>
+				  <item attr="foo">foobar</item>
+				</root>
+				""")
+
+		filter = XMLConfig(content)
+		filter.pretty()
+		self.assertEqual(filter.content, expected)
