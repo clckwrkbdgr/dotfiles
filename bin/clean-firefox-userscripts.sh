032ac1aa736d052e56cac36b36def199ec5a67f4
Dixie Flatline <umi0451@gmail.com>
2024-05-11 12:01:26 +0300

    Migrated to filterconf.

--- bin/clean-firefox-userscripts.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if [ "$1" = "--restore" ]; then
-	sed 's|$HOME|'"$HOME"'|g'
-	exit
-fi
-sed 's/installTime="[0-9]\+"/installTime="0"/' | \
-sed 's/modified="[0-9]\+"/modified="0"/' | \
-sed 's|'"$HOME"'|$HOME|g'
