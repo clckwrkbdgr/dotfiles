b0f6e3b5c3bc40cbb4e1b848937959d55e559490
Dixie Flatline <umi0451@gmail.com>
2024-07-22 11:08:11 +0300

    Independence of unittest script (but still dependent on unittest module and coverage package, sadly).

--- lib/clckwrkbdgr/rogue/__main__.py
+++ lib/clckwrkbdgr/rogue/__main__.py
@@ -6,6 +6,20 @@ def cli():
 	debug = '--debug' in sys.argv
 	if debug:
 		Log.init('rogue.log')
+	if 'test' in sys.argv:
+		import os, subprocess
+		os.chdir(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+		tests = [arg for arg in sys.argv if arg.startswith('clckwrkbdgr.rogue.')]
+		if not tests:
+			for rootdir, dirnames, filenames in os.walk('clckwrkbdgr/rogue'):
+				for filename in filenames:
+					if filename.startswith('test') and filename.endswith('.py'):
+						tests.append(os.path.join(rootdir, filename))
+		command = ['python', '-m', 'coverage', 'run', '-m', 'unittest']
+		if debug:
+			command.append('--verbose')
+		subprocess.check_call(command + tests)
+		sys.exit()
 	Log.debug('started')
 	game.run()
 	Log.debug('exited')
