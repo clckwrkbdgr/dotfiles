510f46ab794fb26f8208058d910c8584631067ee
Dixie Flatline <umi0451@gmail.com>
2024-07-23 15:40:54 +0300

    Automatically re-running unit tests after every change in source code to ensure stability.

--- lib/clckwrkbdgr/rogue/__main__.py
+++ lib/clckwrkbdgr/rogue/__main__.py
@@ -2,11 +2,23 @@ import sys
 from .messages import Log
 from . import game
 
+def files_have_changed():
+	import os
+	savefile = os.path.expanduser('~/.rogue.sav') # TODO take from game module
+	last_save = os.stat(savefile).st_mtime
+	for rootdir, dirnames, filenames in os.walk(os.path.dirname(__file__)):
+		for filename in filenames:
+			filename = os.path.join(rootdir, filename)
+			if os.stat(filename).st_mtime > last_save:
+				print('Source file {0} has been changed.'.format(filename))
+				return True
+	return False
+
 def cli():
 	debug = '--debug' in sys.argv
 	if debug:
 		Log.init('rogue.log')
-	if 'test' in sys.argv:
+	if 'test' in sys.argv or files_have_changed():
 		import os, subprocess
 		os.chdir(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 		tests = [arg for arg in sys.argv if arg.startswith('clckwrkbdgr.rogue.')]
@@ -22,7 +34,8 @@ def cli():
 		if rc != 0:
 			sys.exit(rc)
 		rc = subprocess.call(['python', '-m', 'coverage', 'report', '-m'])
-		sys.exit(rc)
+		if rc != 0 or 'test' in sys.argv:
+			sys.exit(rc)
 	Log.debug('started')
 	game.run()
 	Log.debug('exited')
