90428e874e20466204f2ecab6daeeb11ae68976d
Dixie Flatline <umi0451@gmail.com>
2024-08-13 11:39:53 +0300

    Tests for tests.

--- lib/dotrogue/__main__.py
+++ lib/dotrogue/__main__.py
@@ -1,61 +1,18 @@
 import os, sys
 from .messages import Log
 from . import game
-
-class Tester:
-	def __init__(self):
-		self.all_tests = []
-	def iter_files(self):
-		os.chdir(os.path.dirname(os.path.dirname(__file__)))
-		for rootdir, dirnames, filenames in os.walk('dotrogue'):
-			if '__pycache__' in rootdir:
-				continue
-			for filename in filenames:
-				if filename.endswith('.pyc'):
-					continue
-				full_filename = os.path.join(rootdir, filename)
-				if filename.startswith('test') and filename.endswith('.py'):
-					self.all_tests.append(full_filename)
-				yield full_filename
-	def need_tests(self, argv):
-		if 'test' in argv:
-			return True
-		if not game.Savefile.exists():
-			return False
-		last_save = game.Savefile.last_save_time()
-		result = False
-		for filename in self.iter_files():
-			if os.stat(filename).st_mtime > last_save:
-				print('Source file {0} has been changed.'.format(filename))
-				result = True
-		return result
-	def get_tests(self, argv):
-		tests = [arg for arg in argv if arg.startswith('dotrogue.')]
-		if not tests:
-			if not self.all_tests:
-				list(self.iter_files())
-			tests = self.all_tests
-		return tests
-	def run(self, tests, debug=False):
-		command = ['python', '-m', 'coverage', 'run', '-m', 'unittest']
-		if debug:
-			command.append('--verbose')
-		import subprocess
-		rc = subprocess.call(command + tests)
-		if rc != 0:
-			sys.exit(rc)
-		rc = subprocess.call(['python', '-m', 'coverage', 'report', '-m'])
-		if rc != 0 or 'test' in sys.argv:
-			sys.exit(rc)
+from .test.main import Tester
 
 def cli():
 	debug = '--debug' in sys.argv
 	if debug:
 		Log.init('rogue.log')
-	tester = Tester()
-	if tester.need_tests(sys.argv):
+	tester = Tester(rootdir=os.path.dirname(os.path.dirname(__file__)))
+	if tester.need_tests(sys.argv, game.Savefile.last_save_time() if game.Savefile.exists() else -1):
 		tests = tester.get_tests(sys.argv)
-		tester.run(tests, debug=debug)
+		rc = tester.run(tests, debug=debug)
+		if rc != 0 or 'test' in sys.argv:
+			sys.exit(rc)
 	Log.debug('started')
 	game.run()
 	Log.debug('exited')
---
+++ lib/dotrogue/test/main.py
@@ -0,0 +1,46 @@
+import os, sys, subprocess
+from ..messages import Log
+
+class Tester:
+	def __init__(self, rootdir):
+		self.rootdir = rootdir
+		self.all_tests = []
+	def iter_files(self):
+		os.chdir(self.rootdir)
+		for rootdir, dirnames, filenames in os.walk('dotrogue'):
+			if '__pycache__' in rootdir:
+				continue
+			for filename in filenames:
+				if filename.endswith('.pyc'):
+					continue
+				full_filename = os.path.join(rootdir, filename)
+				if filename.startswith('test') and filename.endswith('.py'):
+					self.all_tests.append(full_filename)
+				yield full_filename
+	def need_tests(self, argv, last_save):
+		if 'test' in argv:
+			return True
+		if last_save <= 0:
+			return False
+		result = False
+		for filename in self.iter_files():
+			if os.stat(filename).st_mtime > last_save:
+				Log.info('Source file {0} has been changed.'.format(filename))
+				result = True
+		return result
+	def get_tests(self, argv):
+		tests = [arg for arg in argv if arg.startswith('dotrogue.')]
+		if not tests:
+			if not self.all_tests:
+				list(self.iter_files())
+			tests = self.all_tests
+		return tests
+	def run(self, tests, debug=False):
+		command = ['python', '-m', 'coverage', 'run', '--source', 'dotrogue', '-m', 'unittest']
+		if debug:
+			command.append('--verbose')
+		rc = subprocess.call(command + tests)
+		if rc != 0:
+			return rc
+		rc = subprocess.call(['python', '-m', 'coverage', 'report', '-m'])
+		return rc
