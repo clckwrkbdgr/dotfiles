485bd0d75051c268f9872f626114fb8fe41beab4
Dixie Flatline <umi0451@gmail.com>
2025-11-28 17:05:52 +0200

    Fixed running wrapper script under PyDroid studio.

--- .rogue/pydroid.py
+++ .rogue/pydroid.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import os, sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'lib'))
-sys.path.append(os.path.join(os.path.dirname(__file__)))
 import rogue
 rogue.cli(['--gui'])
+""" Without following line PyDroid will not start it as GUI for some insane reason:
+from kivy
+"""
